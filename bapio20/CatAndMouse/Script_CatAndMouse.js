12ce63055388febee919e20485674c1964d89f07
Piofret <baptiste.piofret@free.fr>
2020-02-11 21:27:00 +0100

    modifs

--- bapio20/CatAndMouse/Script_CatAndMouse.js
+++ bapio20/CatAndMouse/Script_CatAndMouse.js
@@ -1,8 +1,12 @@
-var myGamePiece;
+var mouse, cat, cheese1, cheese2, cheese3;
 
 function startGame() {
-    myGamePiece = new component(30, 30, "darkblue", Math.random()*970, Math.random()*570);
     myGameArea.start();
+    mouse = new component(30, 30, "darkblue", Math.random()*970, Math.random()*570);
+    cat = new component(45, 45, "red", Math.random()*955, Math.random()*555);
+    cheese1 = new component(20, 20, "orange", Math.random()*980, Math.random()*580);
+    cheese2 = new component(20, 20, "orange", Math.random()*980, Math.random()*580);
+    cheese3 = new component(20, 20, "orange", Math.random()*980, Math.random()*580);
 }
 
 var myGameArea = {
@@ -13,25 +17,59 @@ var myGameArea = {
         this.context = this.canvas.getContext("2d");
         document.body.insertBefore(this.canvas, document.body.childNodes[0]);
         this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function (e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function (e) {
+            myGameArea.key = false;
+        })
     },
-    clear : function() {
+    clear : function(){
         this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
     }
 }
 
 function component(width, height, color, x, y) {
+    this.gamearea = myGameArea;
     this.width = width;
     this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
     this.x = x;
     this.y = y;
-    this.update = function(){
+    this.update = function() {
         ctx = myGameArea.context;
         ctx.fillStyle = color;
         ctx.fillRect(this.x, this.y, this.width, this.height);
     }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+function remove_cheese(x,y) {
+  hide = new component(20,20,"white",x,y);
 }
 
 function updateGameArea() {
     myGameArea.clear();
-    myGamePiece.update();
+    mouse.speedX = 0;
+    mouse.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 37) {mouse.speedX = -5; }
+    if (myGameArea.key && myGameArea.key == 39) {mouse.speedX = 5; }
+    if (myGameArea.key && myGameArea.key == 38) {mouse.speedY = -5; }
+    if (myGameArea.key && myGameArea.key == 40) {mouse.speedY = 5; }
+    mouse.newPos();
+    mouse.update();
+    cat.speedX = (15*Math.sign(mouse.x-cat.x))/Math.sqrt(50);
+    cat.speedY = (15*Math.sign(mouse.y-cat.y))/Math.sqrt(50);
+    cat.newPos();
+    cat.update();
+    if (-20<(cheese1.x-mouse.x)<30 && -20<(cheese1.y-mouse.y)<30) {}
+    if (-20<(cheese2.x-mouse.x)<30 && -20<(cheese2.y-mouse.y)<30) {}
+    if (-20<(cheese3.x-mouse.x)<30 && -20<(cheese3.y-mouse.y)<30) {}
+    cheese1.update();
+    cheese2.update();
+    cheese3.update();
 }
