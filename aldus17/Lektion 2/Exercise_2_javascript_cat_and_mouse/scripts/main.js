5bcc5af514c9b42b12e19890e62350a388d6a690
AleksanderGD <agdbusiness@hotmail.com>
2020-02-13 16:05:09 +0100

    update to css exercise 1 and javascript mouse and cat.

--- aldus17/Lektion 2/Exercise_2_javascript_cat_and_mouse/scripts/main.js
+++ aldus17/Lektion 2/Exercise_2_javascript_cat_and_mouse/scripts/main.js
@@ -12,33 +12,65 @@ screen
 ▪ When the cat touches the mouse, the mouse has lost
 ▪ Extra challenge: pause/unpause with “p”
 */
-var canvas = document.getElementById("canvas");
-var ctx = canvas.getContext("2d");
+var myGamePiece;
 
 function startGame() {
-    mouse = new createSquare(30, 30, "blue", 10, 10)
-    cat = new createSquare(30, 30, "red", 10, 10)
-
-    chesse = new createSquare(getRndInteger(0, 200), getRndInteger(0, 150), "orange", 10, 10)
+    myGameArea.start();
+    myGamePiece = new component(30, 30, "red", 10, 120);
 }
 
 function getCanvas() {
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    return ctx;
-
+    canvas = document.getElementById("canvas");
+    ctx = canvas.getContext("2d");
 }
 
-function createSquare(posX, posY, color, width, height) {
-
-    ctx = getCanvas();
-    ctx.rect(posX, posY, width, height);
-    ctx.fillStyle = color;
-    ctx.fill();
-
-
+var myGameArea = {
+    canvas = getCanvas(),
+    start: function() {
+        this.canvas.width = 480;
+        this.canvas.height = 270;
+        this.context = this.canvas.getContext("2d");
+        document.body.insertBefore(this.canvas, document.body.childNodes[0]);
+        this.interval = setInterval(updateGameArea, 20);
+        window.addEventListener('keydown', function(e) {
+            myGameArea.key = e.keyCode;
+        })
+        window.addEventListener('keyup', function(e) {
+            myGameArea.key = false;
+        })
+    },
+    clear: function() {
+        this.context.clearRect(0, 0, this.canvas.width, this.canvas.height);
+    }
 }
 
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+function component(width, height, color, x, y) {
+    this.gamearea = myGameArea;
+    this.width = width;
+    this.height = height;
+    this.speedX = 0;
+    this.speedY = 0;
+    this.x = x;
+    this.y = y;
+    this.update = function() {
+        ctx = myGameArea.context;
+        ctx.fillStyle = color;
+        ctx.fillRect(this.x, this.y, this.width, this.height);
+    }
+    this.newPos = function() {
+        this.x += this.speedX;
+        this.y += this.speedY;
+    }
+}
+
+function updateGameArea() {
+    myGameArea.clear();
+    myGamePiece.speedX = 0;
+    myGamePiece.speedY = 0;
+    if (myGameArea.key && myGameArea.key == 37) { myGamePiece.speedX = -1; }
+    if (myGameArea.key && myGameArea.key == 39) { myGamePiece.speedX = 1; }
+    if (myGameArea.key && myGameArea.key == 38) { myGamePiece.speedY = -1; }
+    if (myGameArea.key && myGameArea.key == 40) { myGamePiece.speedY = 1; }
+    myGamePiece.newPos();
+    myGamePiece.update();
 }
