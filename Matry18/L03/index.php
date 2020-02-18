e1e1838684fb25bafd2026e71ef5a989dcdd82e0
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-02-18 20:18:25 +0100

    Løst
    user="username"
    pw="password"

--- Matry18/L03/index.php
+++ Matry18/L03/index.php
@@ -1,31 +1,45 @@
-<form method="post">
-            
-    <legend>Fill in the form to log in:</legend>   
-    <label for="name">Name:</label>
-    <input type="text" id="name" name="name"/>
-    <br>
-    <label for="password">Password:</label>
-    <input type="password" id="password" name="password"/>
-    <br>
-    <input type="submit" value="submit button"/>
-</form>
+<html>
+    <head>
+        <title>PHP Post Form User Login</title>
+    </head>
+    <body>
+        <div id="main">
+            <center>
+                    <h1>User login</h1>
+                    <form method="POST">            
+                        <legend>Fill in the form to log in:</legend>   
+                        <label for="username">Username:</label>
+                        <input type="text" name="username" autocomplete="off" required>
+                        
+                        <label for="password">Password:</label>
+                        <input type="password" name="password" required>
+                        <br>
+                        <input type="Submit" name="submit" value="Login" id="submit"/>
+                    </form>
+                </center>
+        </div>
+    </body>
+</html>
 
 <?php
-$message = "You are not logged in!";
-echo $message;
+    if (isset($_POST['submit'])) {
+        $un=$_POST['username'];
+        $pw=$_POST['password'];
+        $wrongpw="Invalid password!";
+        $wrongun="Invalid username!";
 
-$loggedIn = False;
-if($_POST["name"] == "Mathias") {
-    $message = "Welcome Mathias!";
-    echo $message;
-    $loggedIn = True;
-    
+        if ($un=='username' && $pw=='password') {
+            header("location:home.html");
+            exit();
+        } elseif ($un != 'username' && $pw == 'password') {
+           echo "<p align=center>$wrongun </p>";
+        } elseif ($un=='username' && $pw!='password') {
+           echo "<p align=center>$wrongpw </p>";
+        }
+        else
+            echo "<p align=center>$wrongun And $wrongpw </p>";
+    }
 
-} else {
-    $message = "Wrong user!";
-    echo $message;
-    $loggedIn = False;
-    echo $loggedIn;
-}
 ?>
+
 
