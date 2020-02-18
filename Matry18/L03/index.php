f7ef38b76f5d1d25b53640a969b104c1501fcaa1
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-02-18 13:47:39 +0100

    php

--- Matry18/L03/index.php
+++ Matry18/L03/index.php
@@ -1,24 +1,31 @@
-
 <form method="post">
             
     <legend>Fill in the form to log in:</legend>   
-    <label for="name">Name</label>
+    <label for="name">Name:</label>
     <input type="text" id="name" name="name"/>
     <br>
-    <label for="password">Password</label>
-    <input type="text" id="password" name="password"/>
+    <label for="password">Password:</label>
+    <input type="password" id="password" name="password"/>
     <br>
     <input type="submit" value="submit button"/>
 </form>
 
 <?php
+$message = "You are not logged in!";
+echo $message;
+
 $loggedIn = False;
 if($_POST["name"] == "Mathias") {
-    echo 'Hej Mathias';
+    $message = "Welcome Mathias!";
+    echo $message;
     $loggedIn = True;
+    
+
 } else {
-    echo 'Wrong user';
+    $message = "Wrong user!";
+    echo $message;
     $loggedIn = False;
+    echo $loggedIn;
 }
 ?>
 
