b65251c9709026dcf839b3ef4ac6f1acc3534d33
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-02-25 11:01:32 +0100

    php db

---
+++ Matry18/L04/index.php
@@ -0,0 +1,18 @@
+<html>
+    <head>
+        <title>Look up in DB</title>
+    </head>
+    <body>
+        <div id="main">
+            <center>
+                    <h1>DB lookup</h1>
+                    <form method="POST">            
+                        <legend>Search for book, authors or publishers:</legend>   
+                        <label for="search">Search:</label>
+                        <input type="text" name="search">
+                        <input type="Submit" name="submit" value="Search" id="submit"/>
+                    </form>
+                </center>
+        </div>
+    </body>
+</html>
