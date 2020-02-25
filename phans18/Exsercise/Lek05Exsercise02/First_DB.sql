b709d9012445ecd973097e8b4e028ea73e00bf8e
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-02-25 09:33:01 +0100

    Create First_DB.sql

---
+++ phans18/Exsercise/Lek05Exsercise02/First_DB.sql
@@ -0,0 +1,14 @@
+CREATE TABLE Auther (
+	Id INT PRIMARY KEY AUTO_INCREMENT,
+    Auther_name VARCHAR(20) UNIQUE,
+    Auther_title VARCHAR(30) NOT NULL
+);
+
+INSERT INTO Auther(Auther_name, Auther_title) VALUE ('Mr. Test 1', 'The Answer is 42');
+INSERT INTO Auther(Auther_name, Auther_title) VALUE ('Mr. Test 2', 'Why the Answer is not 42');
+
+SELECT * FROM Auther;
+SELECT * FROM Auther WHERE Auther_title LIKE '%42%';
+
+
+DROP TABLE AUTHER;
