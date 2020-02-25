7a85955e29cc5aad25301382a01ba67ea4808436
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-02-25 12:39:44 +0100

    Løst

--- Matry18/L04/index.php
+++ Matry18/L04/index.php
@@ -1,14 +1,14 @@
 <html>
     <head>
         <title>Look up in DB</title>
+        <meta charset = "UTF-8">
     </head>
     <body>
         <div id="main">
             <center>
                     <h1>DB lookup</h1>
-                    <form method="POST">            
+                    <form method="POST" action="pdo.php">            
                         <legend>Search for book, authors or publishers:</legend>   
-                        <label for="search">Search:</label>
                         <input type="text" name="search">
                         <input type="Submit" name="submit" value="Search" id="submit"/>
                     </form>
--- Matry18/L04/pdo.php
+++ Matry18/L04/pdo.php
@@ -1,16 +1,38 @@
 <?php
 $servername = "localhost:3307";
 $username = "root";
-$password = "udfyldes";
+$password = "KatharinaOdens3";
+
+$searchItem = $_POST['search'];
 
 try {
-    $conn = new PDO("mysql:host=$servername;dbname=myDB", $username, $password);
+    $conn = new PDO("mysql:host=$servername;dbname=search", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-    echo "Connected successfully";
+    echo "Search result:";
     }
 catch(PDOException $e)
     {
     echo "Connection failed: " . $e->getMessage();
     }
+
+    $sql = "SELECT * 
+            FROM book 
+            WHERE book_title LIKE '%$searchItem%' 
+            OR author_name LIKE '%$searchItem%' 
+            OR publisher_name LIKE '%$searchItem%';";
+    $stmt = $conn->prepare( $sql );
+    $stmt->execute();
+    $result = $stmt->fetchAll();
+    foreach ($result as $row) {
+        echo "<br />\n"."Book title: ".$row['book_title']."<br />\n".
+            "Author name: ".$row['author_name']."<br />\n".
+            "Publisher: ".$row['publisher_name']."<br />\n";
+    }
+
+
+    
+
+
+
 ?> 
