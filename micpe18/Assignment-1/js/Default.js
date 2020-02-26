8c15cbd5a122b6c96c91dbcf95ac2390f733e1ae
Michael <kvik098@gmail.com>
2020-02-26 08:39:26 +0100

    css grid started

---
+++ micpe18/Assignment-1/js/Default.js
@@ -0,0 +1,8 @@
+function openSlideMenu() {
+    document.getElementById("menu").style.width = "250px";
+    document.getElementById("content").style.marginLeft = "250px";
+  }
+  function closeSlideMenu() {
+    document.getElementById("menu").style.width = "0";
+    document.getElementById("content").style.marginLeft = "0";
+  }
