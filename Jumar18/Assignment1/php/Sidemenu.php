89576e8a7ecb3e4eb9896cb19158f2ab92394f1c
Michael <kvik098@gmail.com>
2020-02-26 08:52:35 +0100

    Revert "test"
    This reverts commit cdcdee43f6083d537006edf4cfdf3ab9d8dea174.

---
+++ Jumar18/Assignment1/php/Sidemenu.php
@@ -0,0 +1,11 @@
+<div class="itemmenu">
+    <ul class="sidenav">
+        <li><a href="#">Frontpage</a></li>
+        <li><a href="#">Image feed</a></li>
+        <li><a href="#">Upload page</a></li>
+        <li><a href="#">Users</a></li>
+        <li>
+            <a href="#" onclick="document.getElementById('formid').style.display='flex'">Login</a>
+        </li>
+    </ul>
+</div>
