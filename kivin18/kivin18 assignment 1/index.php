5b3ebccebf2bcd8306509c40ae07beb304ad2df7
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-02-26 10:34:19 +0100

    Update login form, add grid layout and style.css

--- kivin18/kivin18 assignment 1/index.php
+++ kivin18/kivin18 assignment 1/index.php
@@ -5,6 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="css/style.css">
     <title>Document</title>
 </head>
 <body>
@@ -28,22 +29,41 @@ if ($username == $correct_username && $password == $correct_password) {
 
 if ($_SESSION["logged_in"]) :
     ?>
-    <form method="post">
-        <input type="submit" value="Log out"/>
-    </form>
+    <div class="grid-container">
+        <div class="header" name="title" id="title">Image feed</div>
+        <div class="menu">
+            <button class="menuButton" name="imagesButton" id="imagesButton">Images</button>
+            <button class="menuButton" name="usersButton" id="usersButton">Users</button>
+            <button class="menuButton" name="uploadButton" id="uploadButton">Upload</button>
+            <form name="logOut" id="logOut" method="post">
+                <button class="menuButton" value="Log out" type="submit">Log out :'(</button>
+            </form>
+        </div>
+        <div class="main">
+            <div class="content" name="imageFeed" id="imageFeed">Images</div>
+            <div class="content" name="users" id="users">Users</div>
+            <div class="content" name="upload" id="upload">Upload</div>
+        </div>
+        <div class="footer">2020</div>
+    </div>
 <?php else : ?>
-    <form id="form" name="form" method="post">
-        <label for="username">Name</label>
-        <br>
-        <input type="text" name="username" id="username"/>
-        <label class="inputInfo" id="invalidName"></label>
-        <br>
-        <label for="password">Password</label>
-        <br>
-        <input type="password" name="password" id="password"/>
-        <label class="inputInfo" id="invalidPass"></label>
-        <input type="submit"/>
-    </form>
+    <!-- Login -->
+    <div id="loginForm" name="loginForm">
+        <form id="login" name="login" method="post">
+            <label for="username">Username</label>
+            <br/>
+            <input type="text" placeholder="Enter username" name="username" id="username" required/>
+            <br/>
+            <label for="password">Password</label>
+            <br/>
+            <input type="password" placeholder="Enter password" name="password" id="password" required/>
+            <br/>
+            <button value="Login" type="submit">Login</button>
+            <br/>
+            <label name="wrongInfo" id="wrongInfo"></label>
+        </form>
+        <button id="registerButton">Sign Up</button>
+    </div>
 <?php endif; ?>
 </body>
 </html>
