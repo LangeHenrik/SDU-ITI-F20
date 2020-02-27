431dc56775fdd551cfea5ef8c452f25bc2db544b
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-02-27 16:48:35 +0100

    Add db connection and config

---
+++ kivin18/kivin18 assignment 1/db_config.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    "host" => "localhost",
+    "db" => "iti",
+    "user" => "root",
+    "pass" => ""
+];
--- kivin18/kivin18 assignment 1/index.php
+++ kivin18/kivin18 assignment 1/index.php
@@ -11,6 +11,25 @@
 <body>
 
 <?php
+$config = require('db_config.php');
+$host = $config["host"];
+$db   = $config["db"];
+$user = $config["user"];
+$pass = $config["pass"];
+
+$dsn = "mysql:host=$host;dbname=$db";
+$options = [
+    PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
+    PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
+    PDO::ATTR_EMULATE_PREPARES   => false,
+];
+
+try {
+    $pdo = new PDO($dsn, $user, $pass, $options);
+} catch (PDOException $e) {
+   echo "Error: " .$e->getMessage();
+}
+
 $correct_username = "john";
 $correct_password = "1234";
 
