cf7456dfa3a379001670500e60f9d538a488e7d8
FrederikH <f.haagensen@hotmail.com>
2020-02-27 10:31:27 +0100

    loginbeginning

---
+++ kaseb18frhaa18/Login.php
@@ -0,0 +1,30 @@
+<html lang = "en">
+<head>
+  <!-- basic.html -->
+  <title>Title of the document</title>
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<script src="myscripts.js"></script>
+<link rel="stylesheet" type="text/css" href="style.css">
+  <meta charset = "UTF-8" />
+</head>
+<body>
+    <div class="header">
+        <ul class="menu">
+        <li> <a href=#>Login</a></li>
+        <li> <a href=#>Register</a></li>
+        </ul>
+    </div>
+    <div class="wrapper">
+        <div class="content">
+            <form action="submit">
+                <label for=""></label>
+                <input type="text" name="username">
+                <br/>
+                <input type="password" name="password">
+                <button type="submit" value="Log in"></button>
+            </form>
+        </div>
+    </div>
+
+</body>
+</html>
