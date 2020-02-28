62d35c84621778a2788505150be0dcc4579f1dee
Jacob <Jacso18@student.sdu.dk>
2020-02-28 23:00:57 +0100

    css

--- Jacso18/Assignment_01/image_feed.php
+++ Jacso18/Assignment_01/image_feed.php
@@ -1,54 +1,56 @@
 <?php
-    include 'includes/autoload.php';
+include 'includes/autoload.php';
 
 ?>
 <?php
-    session_start();
-            
-    $userController = new UserController();
-    $posts = $userController->getAllPosts();
+session_start();
 
-    Utility::redirectIfNotLoggedIn();
-    Utility::logoutPressed();
+$userController = new UserController();
+$posts = $userController->getAllPosts();
+
+Utility::redirectIfNotLoggedIn();
+Utility::logoutPressed();
 ?>
 
 <!DOCTYPE html>
-<html>   
-    <head>
-        <title>Image Feed</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="/CSS/stylesheet.css">
-        <html lang="en">        
-    </head>
-    <body>
-    <nav>   
-        <div class="center">    
-            
-        <ul class="menu">
+<html>
+
+<head>
+    <title>Image Feed</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/CSS/stylesheet.css">
+    <html lang="en">
+</head>
+
+<body>
+    <nav>
+        <div class="center">
+            <ul class="menu">
                 <li><a href="image_feed.php">Image feed</a></li>
                 <li><a href="upload.php">Upload picture</a></li>
                 <li><a href="user_list.php">Users</a></li>
-        </ul>
-</div>
-</nav>
-        <div class="wrapper">
-            <div class= "content">
-                <?php foreach($posts as $post){?>
+            </ul>
+        </div>
+    </nav>
+    <div class="wrapper">
+        <div class="content">
+            <?php foreach ($posts as $post) { ?>
                 <div class="post">
-                    <h1><?php echo $post['title'];?></h1>
+                    <h1><?php echo $post['title']; ?></h1>
                     <p><?php echo 'Posted by: ' . $post['username'] . ' at ' . $post['timestamp']; ?> </p>
                     <img src=<?php echo $post['image']; ?> />
                     <p><?php echo $post['COMMENT']; ?></p>
-                    <br/>
+                    <br />
                 </div>
-                <?php
-                }?>
-            </div>
+            <?php
+            } ?>
         </div>
-        
-        <form method="POST">
-            <input type="submit" name="logout" id="logout" value="Logout"/>
-        </form>
-    </body>
+    </div>
+
+    <form method="POST">
+        <input type="submit" name="logout" id="logout" value="Logout" />
+    </form>
+</body>
+
 </html>
--- Jacso18/Assignment_01/upload.php
+++ Jacso18/Assignment_01/upload.php
@@ -1,81 +1,83 @@
 <?php
-    include 'includes/autoload.php';
+include 'includes/autoload.php';
 
 ?>
 
 <!DOCTYPE html>
-<html>   
-    <head>
-        <title>Upload image</title>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-        <link rel="stylesheet" type="text/css" href="/CSS/stylesheet.css"> 
-    </head>
-    <body>
-        <nav>   
-        <div class="center">    
+<html>
+
+<head>
+    <title>Upload image</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+    <link rel="stylesheet" type="text/css" href="/CSS/stylesheet.css">
+</head>
+
+<body>
+    <nav>
+        <div class="center">
             <ul class="menu">
                 <li><a href="image_feed.php">Image feed</a></li>
                 <li><a href="upload.php">Upload picture</a></li>
                 <li><a href="user_list.php">Users</a></li>
             </ul>
         </div>
-        </nav>
-        <div class="uploadarea">
-            <form id="uploadform" method="POST" enctype="multipart/form-data">
-                <label for="title">Title</label>
-                <br>
-                <input type="text" name="title" id = "title" >
+    </nav>
+    <div class="wrapper">
+        <div class="uploadform">
+            <form class= uploadform id="uploadform" method="POST" enctype="multipart/form-data">
+                <input type="text" name="title" id="title" placeholder="Title">
                 <br>
                 <label for="file">Select a file:</label>
                 <br>
                 <input type="button" id="loadFile" value="file" onclick="document.getElementById('filetoupload').click();" />
-                <input type="file" style="display:none;" id="filetoupload" name="filetoupload"/>
+                <input type="file" style="display:none;" id="filetoupload" name="filetoupload" />
                 <br>
-                <textarea id="comment" name="comment" rows="4" cols="50" maxlength="150" placeholder="Write a comment"></textarea>
+                <textarea id="comment" name="comment" rows="2" cols="50" maxlength="150" placeholder="Write a comment" style="font-family:sans-serif"></textarea>
                 <br>
                 <input type="submit" name="upload" id="upload" value="upload">
             </form>
         </div>
+    </div>
 
 
-        <form method="POST">
-            <input type="submit" name="logout" id="logout" value="Logout"/>
-        </form>
-        <?php
-            session_start();
+    <form method="POST">
+        <input type="submit" name="logout" id="logout" value="Logout" />
+    </form>
+    <?php
+    session_start();
 
-            if(isset($_POST['upload'])){
-                $usercontroller = new UserController();
-                $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
-                $comment = filter_input(INPUT_POST, "comment", FILTER_SANITIZE_STRING);
-                $date = new DateTime();
+    if (isset($_POST['upload'])) {
+        $usercontroller = new UserController();
+        $title = filter_input(INPUT_POST, "title", FILTER_SANITIZE_STRING);
+        $comment = filter_input(INPUT_POST, "comment", FILTER_SANITIZE_STRING);
+        $date = new DateTime();
 
 
-                $name = $_FILES['filetoupload']['name'];
-                $target_file = basename($_FILES["filetoupload"]["name"]);
-                // Select file type
-                $imageFileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
-                // Valid file extensions
-                $extensions_arr = array("jpg","jpeg","png","gif");
-              
-                // Check extension
-                if(in_array($imageFileType,$extensions_arr) ){
-               
-                  // Convert to base64 
-                  $image_base64 = base64_encode(file_get_contents($_FILES['filetoupload']['tmp_name']) );
-                  $image = 'data:image/'.$imageFileType.';base64,'.$image_base64;
-                  // Insert record
-                  $usercontroller->savePost($_SESSION['username'], $title, $image, $comment);
+        $name = $_FILES['filetoupload']['name'];
+        $target_file = basename($_FILES["filetoupload"]["name"]);
+        // Select file type
+        $imageFileType = strtolower(pathinfo($target_file, PATHINFO_EXTENSION));
+        // Valid file extensions
+        $extensions_arr = array("jpg", "jpeg", "png", "gif");
 
-                } else {
-                    echo 'The file has to be either jpg, jpeg, png or gif';
-                }
-            }
-            Utility::redirectIfNotLoggedIn();
-            Utility::logoutPressed();
+        // Check extension
+        if (in_array($imageFileType, $extensions_arr)) {
 
-        ?>
+            // Convert to base64 
+            $image_base64 = base64_encode(file_get_contents($_FILES['filetoupload']['tmp_name']));
+            $image = 'data:image/' . $imageFileType . ';base64,' . $image_base64;
+            // Insert record
+            $usercontroller->savePost($_SESSION['username'], $title, $image, $comment);
+        } else {
+            echo 'The file has to be either jpg, jpeg, png or gif';
+        }
+    }
+    Utility::redirectIfNotLoggedIn();
+    Utility::logoutPressed();
 
-    </body>
+    ?>
+
+</body>
+
 </html>
