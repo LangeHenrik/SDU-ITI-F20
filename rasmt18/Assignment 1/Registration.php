78217c603a7ecc1a34cf36b3ced8075a70e75825
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-02-28 09:18:45 +0100

    Registration

---
+++ rasmt18/Assignment 1/Registration.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Registration</title>
+</head>
+<body>
+    <div class="content" id="registration">
+        <h1>Registration</h1>
+            <form method="POST" action=>
+                <fieldset>            
+                    <legend>Registration for system:</legend>   
+                    <input type="text" name="username" placeholder="Write username here" autofocus>
+                    <br>
+                    <input type="password" name="password" placeholder="Write password here" >
+                    <br>
+                    <input type="password" name="password2" placeholder="Write password again" >
+                    <br>
+                    <input type="Submit" name="submit" value="Register">
+                    
+                </fieldset>
+            </form>
+
+    </div>
+</body>
+</html>
