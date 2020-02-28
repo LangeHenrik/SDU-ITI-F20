c6005924cfca1995669856184c46c3d5c3c57226
Rasmus Thomsen <rasmt18@student.sdu.dk>
2020-02-28 09:17:38 +0100

    Initial post for Assignment 1

---
+++ rasmt18/Assignment 1/index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="./css/styling.css">
+    <title>Front Page</title>
+</head>
+<body>
+    <div class="content">
+        <h1>ITI - Assignment 1</h1>
+        <p>Group Members</p>
+        <ul>
+            <li>Mathias Tryggedsson</li>
+            <li>Rasmus Thomsen</li>
+            <li>Søren Pederson</li>
+        </ul>
+        <form class="login" name="login" method="POST">
+            <fieldset>
+                <legend>Please enter your credentials to login</legend>
+                <label for="username">Username</label>
+                <br>
+                <input type="text" name="username" autofocus>
+                <br>
+                <label for="password">Password</label>
+                <br>
+                <input type="password" name="password">
+                <br>
+                <input type="submit" name="submit" value="Submit">
+            </fieldset>
+        </form>
+        <p>Don't have an account yet, don't worry. Just enter the registration page in the link below</p>
+        <a href="google.com">Registration page</a>
+    <div>
+</body>
+</html> 
