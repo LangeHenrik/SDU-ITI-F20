d511a56d1ada5f40a0f44883f0f59335aee5f1d8
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-02-28 11:35:26 +0100

    Password RegEx

--- rasmt18/Assignment 1/Registration.php
+++ rasmt18/Assignment 1/Registration.php
@@ -7,30 +7,34 @@
     <title>Registration</title>
 </head>
 <body>
-    <div class="content" id="registration">
-        <h1>Registration</h1>
-            <form method="POST" action=>
-                <fieldset>            
-                    <legend>Registration for system:</legend>   
-                    <label for = "username">Username: </label>
-                    <br>
-                    <input type="text" name="username" placeholder="Write username here" autofocus>
-                    <br>
-                    <label for = "password">Password: </label>
-                    <br>
-                    <input type="password" name="password" placeholder="Write password here" >
-                    <br>
-                    <label for = "password2">Retype password: </label>
-                    <br>
-                    <input type="password" name="password2" placeholder="Write password again" >
-                    <br>
-                    <input type="Submit" name="submit" value="Register">
-                    
-                    
-                </fieldset>
-            </form>
-        <p>If you are having trouble registering, please contact support.</p>
-        <br>
-    </div>
+        <div class="content" id="registration">
+            <h1>Registration</h1>
+                <form onKeyUp="return checkFields()" method="POST" action=>
+                    <fieldset>            
+                        <legend>Registration for system:</legend>   
+                        <label for = "username">Username: </label>
+                        <p id="usernameStatus"> </p>
+                        <br>
+                        <input type="text" name="username" id="username" placeholder="Write username here" autofocus>
+                        <br>
+                        <label for = "password">Password: </label>
+                        <br>
+                        <p id="passwordStatus"> </p>
+                        <input type="password" name="password" id="password" placeholder="Write password here" >
+                        <br>
+                        <label for = "password2">Retype password: </label>
+                        <br>
+                        <p id="password2Status"> </p>
+                        <input type="password" name="password2" id="password2" placeholder="Write password again" >
+                        <br>
+                        <input type="Submit" name="submit" value="Register" id="submit">
+                        
+                        
+                    </fieldset>
+                </form>
+            <p>If you are having trouble registering, please contact support.</p>
+            <br>
+        </div>
+    <script src="regExRegistration.js"></script>
 </body>
 </html>
---
+++ rasmt18/Assignment 1/regExRegistration.js
@@ -0,0 +1,49 @@
+function checkFields()
+{
+    var testUsername = document.getElementById('username').value;
+    var regex = new RegExp(/(?!.*[\.\-\_]{2,})^[a-zA-Z0-9\.\-\_]{3,24}$/gm); //Minimum length (3). Maximum length(24). Can only contain alphanumeric characters and the following special characters: dot (.), underscore(_) and dash (-). The special characters cannot appear more than once consecutively or combined. 
+    if(regex.test(testUsername)){
+
+        document.getElementById('username').style.color = "green";
+        document.getElementById('usernameStatus').innerHTML = "";
+        console.log("username is ok");
+        
+    } else {
+        document.getElementById('username').style.color = "red";
+        document.getElementById('usernameStatus').innerHTML = "Minimum length (3).<br>Maximum length(24).<br>Can only contain alphanumeric characters and the following special characters: dot (.), underscore(_) and dash (-). The special characters cannot appear more than once consecutively or combined.";
+        console.log("Minimum length (3).\nMaximum length(24).\nCan only contain alphanumeric characters and the following special characters: dot (.), underscore(_) and dash (-). The special characters cannot appear more than once consecutively or combined.");
+    }
+    var testPassword = document.getElementById('password').value;
+    //Contains 8 or more chars
+    //At least one lower case char 
+    //At least one upper case char
+    //At least one number
+    var regex = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm); 
+    if(regex.test(testPassword)){
+
+        document.getElementById('password').style.color = "green";
+        document.getElementById('passwordStatus').innerHTML = "";
+        console.log("password is ok");
+        
+    } else {
+        document.getElementById('password').style.color = "red";
+        document.getElementById('passwordStatus').innerHTML = "Password must contain 8 or more characters.<br>At least one lower case character.<br>At least one upper case character.<br>At least one number.";
+        console.log("Password must contain 8 or more characters.\nAt least one lower case character.\nAt least one upper case character.\nAt least one number.");
+    
+    }
+
+    var password = document.getElementById('password').value;
+    var password2 = document.getElementById('password2').value;
+
+    if (password != password2) {
+        document.getElementById('password2').style.color = "red";
+        document.getElementById('password2Status').innerHTML = "Passwords must match";
+        console.log("Passwords does not match!");
+        return false;
+    } else {
+        document.getElementById('password2').style.color = "green";
+        document.getElementById('password2Status').innerHTML = '';
+        console.log("Passwords match.");
+        return true;
+    }
+}
