f554947f4322a2e5c1b0c3ef9072e00d00195709
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2020-02-29 17:51:03 +0100

    Added 1 more page and changed the art style of some pages

--- ahnai17/Assignment1/Login.php
+++ ahnai17/Assignment1/Login.php
@@ -1,5 +1,35 @@
 <?php  
+session_start();
 require_once 'db_config.php';   
+$username = filter_input(INPUT_POST, 'username');
+$password=filter_input(INPUT_POST, 'password');
+try {
+        /* @var $conn PDO */
+    $conn = new PDO("mysql:host=$db_server;dbname=$db_name", $db_user, $db_pass,
+    array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $sql = "SELECT username, password FROM users WHERE username='$username'";
+    $loggin = $conn->prepare($sql);
+    $loggin->bindParam('password', $password);
+    $password_hash=password_hash($password, PASSWORD_DEFAULT);
+    $loggin->execute();
+    $output = 'login error';
 
-header("Location: http://localhost:8080/web_gallery.html");
-exit;
+if ($loggin->execute() && $row = $loggin->fetch())
+{
+    if ( $row['password']===$password)
+    {
+        // Account found
+        if ( $row['activated'] !== 0 ) {
+            $output = 'not activated';
+            $_SESSION['username'] = $username;
+        } else {
+        $output = 'logged in';
+         header("Location: http://localhost:8080/Home.html");
+            exit;
+        }
+    }
+}
+    echo $output;
+    } catch (PDOException $e){
+        echo "Error: " . $e->getMessage();
+    }
