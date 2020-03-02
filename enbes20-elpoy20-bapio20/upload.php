00ba22f3b22aef8712477be373919aa7012699e1
enzobes <enzobes@me.com>
2020-03-02 23:24:44 +0100

    upload image ok

--- enbes20-elpoy20-bapio20/upload.php
+++ enbes20-elpoy20-bapio20/upload.php
@@ -2,38 +2,38 @@
 include('header.php');
 ini_set('display_errors','on');
 error_reporting(E_ALL);
-
 include("config.php");
 
-$date=date("Y-m-d H:i:s");
-$user_id =$_GET['id'];
-$header =$_POST['header'];
-$description =$_POST['description'];
+if (isset($_POST['formUpload'])) {
 
-if(isset($_POST['submit_upload'])){
+  $date=date("Y-m-d H:i:s");
+  $user_id =$_GET['id'];
+  $header =$_POST['header'];
+  $description =$_POST['description'];
 
-  $name = $_FILES['file']['name'];
+    $name = $_FILES['file']['name'];
 
-  // Type of the uploaded file
-  $type = strtolower(pathinfo($name,PATHINFO_EXTENSION));
-  //Check if allowed in array
-  $typeOK = array("jpg","jpeg","png","gif");
+    // Type of the uploaded file
+    $type = strtolower(pathinfo($name,PATHINFO_EXTENSION));
+    //Check if allowed in array
+    $typeOK = array("jpg","jpeg","png","gif");
 
-  //$data = file_get_contents($name);
+    //$data = file_get_contents($name);
 
-  //check if extension exists in array()
-  if(in_array($type,$typeOK)){
-    $img64 = base64_encode(file_get_contents($_FILES['file']['tmp_name']));
-    $image = 'data:image/'.$type.';base64,'.$img64;
-    //$base64 = 'data:image/' . $type . ';base64,' . base64_encode(file_get_contents($_FILES['file']['tmp_name']));
+    //check if extension exists in array()
+    if(in_array($type,$typeOK)){
+      $img64 = base64_encode(file_get_contents($_FILES['file']['tmp_name']));
+      $image = 'data:image/'.$type.';base64,'.$img64;
+      //$base64 = 'data:image/' . $type . ';base64,' . base64_encode(file_get_contents($_FILES['file']['tmp_name']));
 
-    $stmt= $db->prepare("INSERT INTO images (image, header, description, created, user_id) VALUES ( ?, ?, ?, ?, ?)");
-    $stmt->execute(array($image, $header, $description,$date ,$user_id));
-  }
+      $stmt= $db->prepare("INSERT INTO images (image, header, description, created, user_id) VALUES ( ?, ?, ?, ?, ?)");
+      $stmt->execute(array($image, $header, $description,$date ,$user_id));
+      var_dump($stmt);
+    }
 }
 ?>
 <div class="uploadpage">
-  
+
 
   <div class="container" >
     <div class="title">UPLOAD PAGE</div>
@@ -43,9 +43,8 @@ if(isset($_POST['submit_upload'])){
         <input type='text' name='header' />
         <label for="description">Description</label>
         <input type='text' name='description' />
-        <?php echo $description ?>
-
-        <input type='submit' value='Upload Image' name='submit_upload'>
+<?php var_dump($_POST) ?>
+        <input type='submit' value='Upload Image' name='formUpload'>
     </form>
     </div>
   </div>
