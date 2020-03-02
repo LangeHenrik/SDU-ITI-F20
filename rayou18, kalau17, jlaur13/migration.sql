7a4f6bfd39a7490f0ae25576dcdf4dc065668c7c
Kasper Ejby <kasper.ejby@gmail.com>
2020-03-02 09:22:43 +0100

    DB

---
+++ rayou18, kalau17, jlaur13/migration.sql
@@ -0,0 +1,23 @@
+DROP DATABASE IF EXISTS kalau17;
+CREATE DATABASE kalau17;
+USE kalau17;
+
+CREATE TABLE user(
+  user_id int auto_increment,
+	username char(50) not null unique ,
+	password char(255) not null,
+	constraint user_pk
+		primary key (user_id)
+);
+
+create table picture(
+  picture_id int auto_increment,
+	user char(50) not null,
+	header char(50) not null,
+	description char(255) not null,
+	picture blob not null,
+	constraint picture_pk
+		primary key (picture_id),
+	constraint picture_username_fk
+		foreign key (user) references user(username)
+);
