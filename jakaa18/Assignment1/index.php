00cf510484f59708af69b58d5c8e0889ecd9b270
JakobKaae <jakaa18@student.sdu.dk>
2020-03-02 21:27:21 +0100

    Removed require line

--- jakaa18/Assignment1/index.php
+++ jakaa18/Assignment1/index.php
@@ -3,16 +3,17 @@
 <html lang="en">
 
 <head>
-    <meta charset="UTF-8">
-    <title>ITI assignment 1</title>
-    <meta http-equiv="X-UA-Compatible" content="ie-edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <script src="scripts/scripts.js"></script>
-    <link rel="stylesheet" type="text/css" href="css/style.css">
+	<meta charset="UTF-8">
+	<title>ITI assignment 1</title>
+	<meta http-equiv="X-UA-Compatible" content="ie-edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<script src="scripts/scripts.js"></script>
+	<link rel="stylesheet" type="text/css" href="css/style.css">
 </head>
 <h1><?php echo 'Log in or sign up via the Registration button' ?></h1>
+
 <body>
-	
+
 	<!-- Login -->
 	<form action="welcome.php" method="post" onsubmit="return checkLogin()">
 		<p>username: <input type="text" name="username" id="usernameId"></p><br>
@@ -21,15 +22,18 @@
 	</form>
 
 	<button><input action="register.php" type="button" name="register" value="Register"></button>
-	
+
 	<?php
-	require("migration.sql");
+	// require("migration.sql");
 
 	if (session_status() == PHP_SESSION_NONE) {
 		session_start();
 		$_SESSION["logged_in"] = false;
 	}
-	
+
+
+
 	?>
+
 	
 </body>
