5051f901097bbaaf1d4023f26e9fc9e53e950bac
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-03-02 16:47:07 +0100

    Update conection.php

--- Mschr18-Phans18-Mach018/Include/conection.php
+++ Mschr18-Phans18-Mach018/Include/conection.php
@@ -4,6 +4,7 @@ try {
     $conn = new PDO("mysql:host=$servername;$dbname", $username, $password);
     // set the PDO error mode to exception
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    console.log
     echo "Connected successfully";
     }
 catch(PDOException $e)
