77212bc79c73405af22d83ce0ff8a51c8dfd5a20
enzobes <enzobes@me.com>
2020-03-02 06:17:28 +0100

    feed+logout

--- enbes20-elpoy20-bapio20/navUser.php
+++ enbes20-elpoy20-bapio20/navUser.php
@@ -3,6 +3,8 @@
          <ul>
            <li><a href="#">HOME</a></li>
            <li><a href="contact.php">Contacts</a></li>
+           <li><a href="logout.php">Logout</a></li>
+
 
 
          </ul>
