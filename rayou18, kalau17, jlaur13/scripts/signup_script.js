f189a72ba802c018dd83ce01fe75bbf77dcc9dd2
rayou18 <43138817+rayou18@users.noreply.github.com>
2020-03-02 15:44:22 +0100

    refactoring code in signup regex

--- rayou18, kalau17, jlaur13/scripts/signup_script.js
+++ rayou18, kalau17, jlaur13/scripts/signup_script.js
@@ -3,17 +3,26 @@
 
 var passwordRegex = new RegExp(/^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/gm);
 var usernameRegex = new RegExp(/\w{3,}/g);
+
+var usernameInput = document.getElementById("usernameInput");
+var passwordInput = document.getElementById("passwordInput");
+var repeatPasswordInput = document.getElementById("repeatPasswordInput");
+var usernameIsSet = false;
+var passwordIsSet = false;
+var repeatPasswordIsSet = false;
+var sumbmitBtn = document.getElementById("signup_btn");
 function checkPassword(){
-  var usernameInput = document.getElementById("usernameInput");
-  var passwordInput = document.getElementById("passwordInput");
-  var repeatPasswordInput = document.getElementById("repeatPasswordInput");
-  var usernameIsSet = false;
-  var passwordIsSet = false;
-  var repatPasswordIsSet = false;
-  var sumbmitBtn = document.getElementById("signup_btn");
 
 
-  if(usernameRegex.test(usernameInput.value)){
+/*  if(usernameIsSet && passwordIsSet && repeatPasswordIsSet){
+  sumbmitBtn.disabled = false;
+  console.log("set to false");
+  }
+  else {
+    sumbmitBtn.disabled = true;
+  } */
+
+/*  if(usernameRegex.test(usernameInput.value)){
     usernameInput.style.borderColor ="green";
     usernameInput.style.borderWidth  ="3px";
     usernameIsSet = true;
@@ -36,28 +45,41 @@ function checkPassword(){
       passwordInput.style.borderWidth  ="3px";
       passwordIsSet = false;
       return false;
-  }
+  } */
 
   if(repeatPasswordInput.value!="" && repeatPasswordInput.value == passwordInput.value){
     repeatPasswordInput.style.borderColor ="green";
     repeatPasswordInput.style.borderWidth  ="3px";
-    repatPasswordIsSet = true;
+    repeatPasswordIsSet = true;
     return false;
   }
   if(repeatPasswordInput.value!="" && repeatPasswordInput.value != passwordInput.value){
     repeatPasswordInput.style.borderColor ="red";
     repeatPasswordInput.style.borderWidth  ="3px";
-    repatPasswordIsSet = false;
+    repeatPasswordIsSet = false;
     return false;
   }
 
-if(usernameIsSet && passwordIsSet && repatPasswordIsSet){
-sumbmitBtn.disabled = false;
-}
-else {
-  sumbmitBtn.disabled = true;
 }
 
+function regexInputCheck(regExp, input){
+console.log("regexInputCheck");
+  if(regExp.test(input.value)){
+    input.style.borderColor ="green";
+    input.style.borderWidth  ="3px";
+    console.log("true");
+    return false;
+  }
+  if(regExp.test(input.value)==false){  //else wouldnt work
+    input.style.borderColor ="red";
+    input.style.borderWidth  ="3px";
+    console.log("false");
+    return false;
+  }
+}
 
-
+function program(){
+  console.log("program");
+  regexInputCheck(usernameRegex,document.getElementById("usernameInput"));
+  regexInputCheck(passwordRegex, document.getElementById("passwordInput"));
 }
