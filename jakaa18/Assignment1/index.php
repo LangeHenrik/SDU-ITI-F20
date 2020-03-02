d6c3e59ecd1bde6ecacb9e9b02c767184b013449
Jakob-k <jakob.kaae1@gmail.com>
2020-03-02 16:17:18 +0100

    Added initial code to index

---
+++ jakaa18/Assignment1/index.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<?php
+    echo "<br><a href='logout.php'><input type=button value=Logout name=logout></a>";
+?>
+
+<html>
+
+<head>
+    <title>Title of the document</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <script src="scripts/scripts.js"></script>
+    <link rel="stylesheet" type="text/css" href="style.css">
+</head>
