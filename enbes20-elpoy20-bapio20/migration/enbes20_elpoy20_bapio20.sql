b0d0809fa0d7d78a75cbceb54f6533fb484374b9
enzobes <enzobes@me.com>
2020-03-02 16:56:08 +0100

    delete collatation db

--- enbes20-elpoy20-bapio20/migration/enbes20_elpoy20_bapio20.sql
+++ enbes20-elpoy20-bapio20/migration/enbes20_elpoy20_bapio20.sql
@@ -33,7 +33,7 @@ CREATE TABLE `images` (
   `image` longblob NOT NULL,
   `created` datetime NOT NULL,
   `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 -- --------------------------------------------------------
 
@@ -46,7 +46,7 @@ CREATE TABLE `user` (
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+);
 
 --
 -- Déchargement des données de la table `user`
