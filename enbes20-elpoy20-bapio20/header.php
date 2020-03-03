de217bdf0232a7463a8ef6fefb3badbb0f18719f
Piofret <baptiste.piofret@free.fr>
2020-03-03 11:44:05 +0100

    Contact list + CSS

--- enbes20-elpoy20-bapio20/header.php
+++ enbes20-elpoy20-bapio20/header.php
@@ -11,6 +11,7 @@
     @import url('https://fonts.googleapis.com/css?family=Montserrat&display=swap');
     </style>
     <link rel="stylesheet" type="text/css" href="style.css">
+    <link rel="stylesheet" type="text/css" href="contact_style.css">
     <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous"> -->
 
     <title>SDU-ITI-F20</title>
--- enbes20-elpoy20-bapio20/userlist.php
+++ enbes20-elpoy20-bapio20/userlist.php
@@ -11,15 +11,18 @@ include("config.php");
 
 $stmt = "SELECT id_user, username, email FROM user";
 $res = $db->query($stmt);
+print_r($res);
 $output="";
 
 while ($row = $res->fetch()){
 			$output = $output . "
-			<div>
-					<div>". $row['id_user'] ."</div>
+			<div class='contact_card'>
+        <div>
 					<div>". $row['username'] ."</div>
 				 	<div>". $row['email'] ."</div>
 				</div>
+        <br>
+      </div>
 		";
 }
 	 $res->closeCursor(); // Termine le traitement de la requête
@@ -27,10 +30,40 @@ while ($row = $res->fetch()){
 /* close connection */
 
 ?>
-<div class="container">
+<div class="container_userlist">
 
-<h1>All contacts</h1>
-
-	<?php echo($output); ?>
+<div class="title">Contact List</div>
+  <form class="search">
+    Username: <input type="text" onkeyup="showHint(this.value)">
+  </form>
+  <div class="contact_list">
+	   <?php echo($output); ?>
+  </div>
+</div>
 
 </div>
+
+<?php // get the q parameter from URL
+
+$q = $_REQUEST["q"];
+
+$hint = "";
+
+// lookup all hints from array if $q is different from ""
+if ($q !== "") {
+    $q = strtolower($q);
+    $len=strlen($q);
+    foreach($a as $username) {
+        if (stristr($q, substr($username, 0, $len))) {
+            if ($hint === "") {
+                $hint = $username;
+            } else {
+                $hint .= ", $username";
+            }
+        }
+    }
+}
+
+// Output "no suggestion" if no hint was found or output correct values
+echo $hint === "" ? "no suggestion" : $hint;
+?>
