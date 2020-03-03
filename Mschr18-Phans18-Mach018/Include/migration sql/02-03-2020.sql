c9ddea41b3510bf1d2d67ce3f4cfaab1702a0c83
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-03-03 15:25:16 +0100

    Update 02-03-2020.sql

--- Mschr18-Phans18-Mach018/Include/migration sql/02-03-2020.sql
+++ Mschr18-Phans18-Mach018/Include/migration sql/02-03-2020.sql
@@ -6,7 +6,7 @@ CREATE TABLE users (
 	userid INT NOT NULL AUTO_INCREMENT,
     username VARCHAR(10) NOT NULL,
     passw VARCHAR(30) NOT NULL,
-    fuldname VARCHAR(100) NOT NULL,
+    fullname VARCHAR(100) NOT NULL,
     phone VARCHAR(25) NOT NULL,
     email VARCHAR(320) NOT NULL,
     PRIMARY KEY (userid)
