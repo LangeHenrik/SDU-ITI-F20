96abcfad8b3756a8b26a9c52f8f1b48b7859047f
enzobes <enzobes@me.com>
2020-03-03 09:51:35 +0100

    check type file before upload

--- enbes20-elpoy20-bapio20/upload.php
+++ enbes20-elpoy20-bapio20/upload.php
@@ -17,7 +17,8 @@ if (isset($_POST['formUpload'])) {
   $header =$_POST['header'];
   $description =$_POST['description'];
 
-    $name = $_FILES['file']['name'];
+  $name = $_FILES['file']['name'];
+  if(!empty($header) AND !empty($description)) {
 
     // Type of the uploaded file
     $type = strtolower(pathinfo($name,PATHINFO_EXTENSION));
@@ -34,8 +35,16 @@ if (isset($_POST['formUpload'])) {
 
       $stmt= $db->prepare("INSERT INTO images (image, header, description, created, user_id) VALUES ( ?, ?, ?, ?, ?)");
       $stmt->execute(array($image, $header, $description,$date ,$user_id));
-      var_dump($stmt);
+      //var_dump($stmt);
+
     }
+    else {
+      $error="Check your type file (" . $type . ") only ". implode (",",$typeOK) ." are allowed";
+    }
+  }
+  else {
+    $error= "Complete Header and Description";
+  }
 }
 ?>
 <div class="uploadpage">
@@ -46,10 +55,13 @@ if (isset($_POST['formUpload'])) {
     <form method="post" class="form" action="" enctype='multipart/form-data'>
         <input type="file" name="file" id="file" required="required">
         <label for="header">Header</label>
-        <input type='text' name='header' />
+        <input type='text' name='header' value="<?php if(isset($header)) { echo $header; } ?>" />
         <label for="description">Description</label>
-        <input type='text' name='description' />
-<?php var_dump($_POST) ?>
+        <input type='text' name='description' value="<?php if(isset($description)) { echo $description; } ?>"/>
+        <?php
+        if(isset($error)) {
+            echo '<font color="red">'.$error."</font>";
+        }?>
         <input type='submit' value='Upload Image' name='formUpload'>
     </form>
     </div>
