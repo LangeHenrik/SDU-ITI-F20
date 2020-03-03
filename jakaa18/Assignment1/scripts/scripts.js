a99f02cfdcdaa518eca5d6197b7f75c4c20e4730
Jakob-k <jakob.kaae1@gmail.com>
2020-03-03 17:27:11 +0100

    Added styling to logged in page and onclick events to hide or show content

--- jakaa18/Assignment1/scripts/scripts.js
+++ jakaa18/Assignment1/scripts/scripts.js
@@ -2,6 +2,21 @@ document.getElementById("register").addEventListener("click",function () {
     document.getElementById("registerView").style.display = "block";
     document.getElementById("loginView").style.display = "none";
 })
+document.getElementById("homelink").addEventListener("click",function () {
+    document.getElementById("homelink").style.display = "block";
+    document.getElementById("photolink").style.display = "none";
+    document.getElementById("userslink").style.display = "none";
+})
+document.getElementById("photolink").addEventListener("click",function () {
+    document.getElementById("homelink").style.display = "none";
+    document.getElementById("photolink").style.display = "block";
+    document.getElementById("userslink").style.display = "none";
+})
+document.getElementById("userslink").addEventListener("click",function () {
+    document.getElementById("homelink").style.display = "none";
+    document.getElementById("photolink").style.display = "none";
+    document.getElementById("userslink").style.display = "block";
+})
 
 function checkLogin() {
 
