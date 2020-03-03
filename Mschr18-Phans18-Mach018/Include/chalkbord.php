32699956f5050bd38c709aa0bff58c380aec52d0
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-03-03 14:19:55 +0100

    new style

---
+++ Mschr18-Phans18-Mach018/Include/chalkbord.php
@@ -0,0 +1,4 @@
+<div class="chalkboard">
+  <h1>ChalkBoard <i class="fas fa-chalkboard"></i></h1>
+  <h3> <i>BETA V.0.0.1</i></h3>
+</div>
--- Mschr18-Phans18-Mach018/index.php
+++ Mschr18-Phans18-Mach018/index.php
@@ -15,12 +15,9 @@
         include_once('loginnav.php');
       ?>
     </header>
-    <section>
-      
-      <div class="content" id="content">
+    <section id="content">
         <div class="welcomme" id="welcomme">
-          <h1>Welcomme to ChalkBoard <i class="fas fa-chalkboard"></i></h1>
-          <h3> <i>BETA V.0.0.1</i></h3>
+          <h1 id="title">Welcomme to </h1> <?php include_once('Include/chalkbord.php') ?>
           <h2><i class="fas fa-user-friends fa-xs"></i> Connect widt you friends.</h2>
           <h2><i class="fas fa-image fa-xs"></i> Post images.</h2>
           <h2><i class="far fa-comments fa-xs"></i> Share comments.</h2>
@@ -35,7 +32,11 @@
             <input type="submit" name="submit" id="login" value="Log in"/>
           </form>
         </div>
-      </div>
+
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32</p>
+        <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.</p>
+        <p>Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32</p>
 
     </section>
     <footer>
