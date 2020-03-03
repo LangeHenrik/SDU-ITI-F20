f631c1869ed6fa7401cec26fde8f05469bd94369
Jakob-k <jakob.kaae1@gmail.com>
2020-03-03 15:56:22 +0100

    Started function to checkLogin for regex

--- jakaa18/Assignment1/scripts/scripts.js
+++ jakaa18/Assignment1/scripts/scripts.js
@@ -2,3 +2,7 @@ document.getElementById("register").addEventListener("click",function () {
     document.getElementById("registerView").style.display = "block";
     document.getElementById("loginView").style.display = "none";
 })
+
+function checkLogin() {
+
+}
