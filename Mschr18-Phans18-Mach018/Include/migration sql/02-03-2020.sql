92aa00c28b70d56dd92b5f024a3d30ecb6d5f7b3
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-03-03 15:22:12 +0100

    Update 02-03-2020.sql

--- Mschr18-Phans18-Mach018/Include/migration sql/02-03-2020.sql
+++ Mschr18-Phans18-Mach018/Include/migration sql/02-03-2020.sql
@@ -4,12 +4,13 @@ USE iti;
 
 CREATE TABLE users (
 	userid INT NOT NULL AUTO_INCREMENT,
-    username VARCHAR(10),
+    username VARCHAR(10) NOT NULL,
     passw VARCHAR(30) NOT NULL,
     fuldname VARCHAR(100) NOT NULL,
     phone VARCHAR(25) NOT NULL,
     email VARCHAR(320) NOT NULL,
     PRIMARY KEY (userid)
 );
+
 
 
