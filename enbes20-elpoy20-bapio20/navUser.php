61faaaa77514907edf00be143fd26e561c174a2d
elpoy20 <elpoy20@student.sdu.dk>
2020-03-03 08:50:18 +0100

    Contact List

--- enbes20-elpoy20-bapio20/navUser.php
+++ enbes20-elpoy20-bapio20/navUser.php
@@ -3,7 +3,7 @@
        <div >
          <ul>
            <li><a href="#">HOME</a></li>
-           <li><a href="contact.php">Contacts</a></li>
+           <li><a href="userlist.php">Contacts</a></li>
            <li><a href="feed.php">Feed</a></li>
            <li><a href="upload.php?id=<?php echo $_SESSION['id']  ?>">Upload</a></li>
            <li><a href="logout.php">Logout</a></li>
