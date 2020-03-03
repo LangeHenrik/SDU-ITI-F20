3d21a3f93829c58d117af7772cd92ffa8887bd4d
Jakob-k <jakob.kaae1@gmail.com>
2020-03-03 14:36:26 +0100

    Hiding login fields when activating register

--- jakaa18/Assignment1/scripts/scripts.js
+++ jakaa18/Assignment1/scripts/scripts.js
@@ -1,3 +1,4 @@
 document.getElementById("register").addEventListener("click",function () {
     document.getElementById("registerView").style.display = "block";
+    document.getElementById("loginView").style.display = "none";
 })
