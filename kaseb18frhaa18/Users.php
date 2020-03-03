19f5785511116014d65400081cab360e956377b0
FrederikH <f.haagensen@hotmail.com>
2020-03-03 13:48:31 +0100

    Update Users.php

--- kaseb18frhaa18/Users.php
+++ kaseb18frhaa18/Users.php
@@ -6,22 +6,21 @@ require 'database.php';
 $statement = "SELECT name, username FROM person";
 $users = talkToDB($statement);
 
-print_r ($users);
 
 $q = $_REQUEST["q"];
-print_r($q);
 
 $hint = "<tr><th>Name</th>
 <th>Username</th>
 </tr>";
 
 // lookup all hints from array if $q is different from ""
-if ($q !== "") {
+if ($q == "") {
+        $hint .= "<tr> <td> $name[name] </td> <td> $name[username] </td> <tr>";  
+} else{
     $q = filter_var($q, FILTER_SANITIZE_STRING);
     $q = strtolower($q);
     $len=strlen($q);
     foreach($users as $name) {
-        print_r($name);
         if (stristr($q, substr($name[name], 0, $len)) || stristr($q, substr($name[username], 0, $len))) {
             $hint .= "<tr> <td> $name[name] </td> <td> $name[username] </td> <tr>";
 
