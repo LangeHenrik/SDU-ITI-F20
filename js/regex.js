b9e72be746bc0c3743f21cef2091a88ce83784da
YuhengHe <50586123+YuhengHe@users.noreply.github.com>
2020-03-05 16:06:39 +0100

    Update regex.js

--- js/regex.js
+++ js/regex.js
@@ -7,6 +7,8 @@ function validateFields() {
 
     var is_username = false;
     var is_password = false;
+    var is_email = false;
+    var is_pwd_repeat = false;
 
     if (regex.test(username_value) == True) {
         document.getElementById('username-register').style.color = "darkblue";
@@ -24,7 +26,28 @@ function validateFields() {
         is_password = true;
         console.log("Password regex")
     } else {
-        document.getElementById('password-register').style.color = "red";
+        document.getElementById('password-register').style.color = "pink";
         console.log("Password not regex")
+    }
+
+    var email_value = document.getElementById('email-register').value;
+    var regex = new RegExp(/([a-zA-Z0-9_\-\.]+)@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.)|(([a-zA-Z0-9\-]+\.)+))([a-zA-Z]{2,4}|[0-9]{1,3})(\]?)$/gm);
+    if (regex.test(email_value)) {
+        document.getElementById('email-register').style.color = "darkblue";
+        is_email = true;
+        console.log("Email regex")
+    } else {
+        document.getElementById('email-register').style.color = "pink";
+        console.log("Email not regex")
+    }
+
+    var pwd_repeat_value = document.getElementById('pwd-repeat').value;
+    if (pwd_repeat_value == password_value) {
+        document.getElementById('pwd-repeat').style.color = "darkblue";
+        is_email = true;
+        console.log("Password Repeat regex")
+    } else {
+        document.getElementById('pwd-repeat').style.color = "pink";
+        console.log("Password Repeat not regex")
     }
 }
