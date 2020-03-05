c9ba55c152519758228471007aa09297753c70c2
YuhengHe <50586123+YuhengHe@users.noreply.github.com>
2020-03-05 15:23:36 +0100

    registration

--- signup.php
+++ signup.php
@@ -7,10 +7,10 @@
             <div id="formContent">
               <h1>Sign-up</h1>
               <form class="form-signup" action="includes\signup1.php" method="post">
-                <input type="text" name="uid" placeholder="Username">
-                <input type="text" name="mail" placeholder="E-mail">
-                <input type="password" name="pwd" placeholder="Password">
-                <input type="password" name="pwd-repeat" placeholder="Repeat Password">
+                <input type="text" name="username-register" id="username-register" placeholder="Username">
+                <input type="text" name="email-register" id="email-register" placeholder="E-mail">
+                <input type="password" name="password-register" id="password-register" placeholder="Password">
+                <input type="password" name="pwd-repeat" id="pwd-repeat" placeholder="Repeat Password">
                 <button type="submit" name="signup-submit">Sign-up</button>
               </form>
             </div>
