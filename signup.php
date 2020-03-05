b99cf8bdab269781259c300e690723ac3df32a84
vitorsv1 <vitorsv1@al.insper.edu.br>
2020-03-05 17:28:20 +0100

    changes

--- signup.php
+++ signup.php
@@ -6,7 +6,7 @@
           <div class="wrapper-main">
             <div id="formContent">
               <h1>Sign-up</h1>
-              <form class="form-signup" action="includes\signup1.php" method="post">
+              <form onKeyUp="return validateFields()" class="form-signup" method="post">
                 <input type="text" name="username-register" id="username-register" placeholder="Username">
                 <input type="text" name="email-register" id="email-register" placeholder="E-mail">
                 <input type="password" name="password-register" id="password-register" placeholder="Password">
