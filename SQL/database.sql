765e5aca7aa523513337443ad0976dd49c079bcb
YuhengHe <50586123+YuhengHe@users.noreply.github.com>
2020-03-05 15:01:35 +0100

    database

---
+++ SQL/database.sql
@@ -0,0 +1,20 @@
+DROP DATABASE IF EXISTS ass1;
+CREATE DATABASE ass1;
+USE ass1;
+
+CREATE TABLE users
+(
+ userid INT auto_increment primary key,
+ username varchar(50) not null,
+ email varchar(100) not null,
+ pwd varchar(100) not null
+);
+
+CREATE TABLE images
+(
+imageid INT auto_increment primary key,
+header varchar(100) not null,
+description varchar(450) not null,
+username varchar(50) not null,
+image longblob not null
+);
