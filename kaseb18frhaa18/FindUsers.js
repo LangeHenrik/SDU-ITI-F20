c04a095dbd3831a67f2dd737307f5748e526141c
FrederikH <f.haagensen@hotmail.com>
2020-03-05 16:37:55 +0100

    lille ændring

--- kaseb18frhaa18/FindUsers.js
+++ kaseb18frhaa18/FindUsers.js
@@ -1,7 +1,7 @@
 function findUsers(userName) {
 
     var xmlhttp = new XMLHttpRequest();
-    xmlhttp.onreadystatechange = function() {
+    xmlhttp.onreadystatechange = function () {
         if (this.readyState == 4 && this.status == 200) {
             document.getElementById("users").innerHTML = this.responseText;
         }
