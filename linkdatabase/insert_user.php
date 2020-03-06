195783bfb6cec4ef8490e6d1f4792f5fa6ad3035
vitorsv1 <vitorsv1@al.insper.edu.br>
2020-03-06 16:46:07 +0100

    error Column 'username' cannot be null

--- linkdatabase/insert_user.php
+++ linkdatabase/insert_user.php
@@ -4,7 +4,7 @@
         try{
             $connection = new PDO("mysql:host=$server;port=3307;dbname=$database", 
             $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
-            $stmt = $connection->prepare("INSERT INTO users (username, email, pwd) VALUES(:username, :email-register, :pwd-register)");
+            $stmt = $connection->prepare("INSERT INTO users (username, email, pwd) VALUES(:username, :email, :pwd)");
             
             $user = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
             $userXSS = htmlspecialchars($user);
@@ -12,12 +12,11 @@
 
             $email = filter_var($_POST['email-register'], FILTER_SANITIZE_STRING);
             $emailXSS = htmlspecialchars($email);
-            echo $emailXSS;
-            $stmt->bindParam(':email-register', $emailXXS, PDO::PARAM_STR);
+            $stmt->bindParam(':email', $emailXXS, PDO::PARAM_STR);
 
             $pass = filter_var(password_hash($_POST['pwd-register'], PASSWORD_DEFAULT), FILTER_SANITIZE_STRING);
             $passXSS = htmlspecialchars($pass);
-            $stmt->bindParam(':pwd-register', $passXSS, PDO::PARAM_STR);
+            $stmt->bindParam(':pwd', $passXSS, PDO::PARAM_STR);
 
             $stmt->execute();
             $stmt->setFetchMode(PDO::FETCH_ASSOC);
