2abdeaf768a8bab848e7aee3f8d87d7840e1ea87
FrederikH <f.haagensen@hotmail.com>
2020-03-06 13:37:35 +0100

    removed useless function

--- kaseb18frhaa18/database.php
+++ kaseb18frhaa18/database.php
@@ -29,27 +29,5 @@ function talkToDB($statement, $parameters){
     }
 }
 
-function talkToDBpls($statement, $parameter){
-    require_once 'db_config.php';
-    
-    try {
-        $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-        $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-        $stmt = $conn->prepare($statement);
-        $stmt->bindParam(":username", $parameter, PDO::PARAM_STR);
-        $stmt->execute();
-
-    // set the resulting array to associative
-        $stmt->execute();
-        $stmt->setFetchMode(PDO::FETCH_ASSOC);
-        $result = $stmt->fetchAll();
-        return $result;
-
-    }
-    catch(PDOException $e) {
-        echo "Error: " . $e->getMessage();
-    }
-}
-
 $conn = null;
 ?>
