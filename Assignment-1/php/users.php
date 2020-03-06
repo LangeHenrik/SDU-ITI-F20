e9e7e0b4a687ea759ca35bde14b1e600bf9c5b4b
Mazipan <julie.markersen@gmail.com>
2020-03-06 14:17:09 +0100

    users not working

---
+++ Assignment-1/php/users.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>
+      Assignment 1
+    </title>
+    <link
+      rel="stylesheet"
+      href="https://use.fontawesome.com/releases/v5.6.3/css/all.css"
+      integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/"
+      crossorigin="anonymous"
+    />
+    <link rel="stylesheet" type="text/css" href="../css/Stylesheet.css" />
+    
+    <link rel="icon" href="../img/icon.png" type="png" />
+  </head>
+  <body>
+    <div id="content">
+      <span class="slide">
+        <a href="#" onclick="openSlideMenu()">
+          <i id="openMenu" class="fas fa-arrow-circle-right"></i>
+        </a>
+      </span>
+
+      <?php
+      require 'navigation.php';
+      ?>
+      <div class="wrapper">
+        <div id="title">
+          <h1>Userlist</h1>
+        </div>
+        <?php
+        require 'inner-wrapper.php';
+        ?>
+      </div>
+    </div>
+    <?php
+    require 'login.php';
+    ?>
+  </body>
+  <script src="./SDU-ITI-F20/Assignment-1/js/Default.js"></script>
+</html>
