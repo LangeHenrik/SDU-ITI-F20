f454a97ce02a371fc019ccdbb32d174e7522c0c5
vitorsv1 <vitorsv1@al.insper.edu.br>
2020-03-07 14:23:05 +0100

    sign up working perfect with hash

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
 $server = "127.0.0.1";
 $database = "ass1";
-$username = "root";
-$password = "160170";
+$username_database = "root";
+$password_database = "160170";
 #$password = "root";
 ?>
--- linkdatabase/insert_user.php
+++ linkdatabase/insert_user.php
@@ -3,19 +3,22 @@
     if(array_key_exists('signup-submit', $_POST)){
         try{
             $connection = new PDO("mysql:host=$server;port=3307;dbname=$database", 
-            $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+            $username_database, $password_database, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
             $stmt = $connection->prepare("INSERT INTO users (username, email, pwd) VALUES(:username, :email, :pwd)");
             
             $user = filter_var($_POST['username'], FILTER_SANITIZE_STRING);
-            $userXSS = htmlspecialchars($user);
-            $stmt->bindParam(':username', $userXXS, PDO::PARAM_STR);
+            #$user = "user";
+            $userXSS = htmlspecialchars($user, ENT_QUOTES | ENT_SUBSTITUTE, 'utf-8');
+            $stmt->bindParam(':username', $userXSS, PDO::PARAM_STR);
 
             $email = filter_var($_POST['email-register'], FILTER_SANITIZE_STRING);
-            $emailXSS = htmlspecialchars($email);
-            $stmt->bindParam(':email', $emailXXS, PDO::PARAM_STR);
+            #$email = "asds@asds.com";
+            $emailXSS = htmlspecialchars($email, ENT_QUOTES | ENT_SUBSTITUTE, 'utf-8');
+            $stmt->bindParam(':email', $emailXSS, PDO::PARAM_STR);
 
             $pass = filter_var(password_hash($_POST['pwd-register'], PASSWORD_DEFAULT), FILTER_SANITIZE_STRING);
-            $passXSS = htmlspecialchars($pass);
+            #$pass = "senha123";
+            $passXSS = htmlspecialchars($pass, ENT_QUOTES | ENT_SUBSTITUTE, 'utf-8');
             $stmt->bindParam(':pwd', $passXSS, PDO::PARAM_STR);
 
             $stmt->execute();
