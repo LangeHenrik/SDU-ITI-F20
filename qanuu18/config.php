e4cd68e1d91f715231aca188edb618ec75069194
Mehmet Øzer <meuze18@student.sdu.dk>
2020-03-07 02:07:10 +0100

    filer oprettet

---
+++ qanuu18/config.php
@@ -0,0 +1,8 @@
+<?php
+
+$servername = "127.0.0.1";
+$username = "root";
+$password = "mehmetdb8";
+$dbname = "assignmentiti";
+
+?>
---
+++ qanuu18/index.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+
+<head>
+    <title>Sign In or Sign Up</title>
+    <link rel="stylesheet" href="loginpage.css">
+</head>
+
+<body>
+    <div id="frm">
+        <form class="login" name="login" method="POST" action="login.php">
+            <p>
+                <label>Username:</label>
+                <input type="text" id="user" name="user">
+            </p>
+            <p>
+                <label>Password:</label>
+                <input type="password" id="pass" name="pass">
+            </p>
+            <p>
+                <input type="submit" id="btn" value="Login">
+            </p>
+        </form>
+        <form class="registration" name="registration" method="POST" action="registration.php">
+            <p>
+                <input type="submit" id="btn" value="Registration">
+            </p>
+        </form>
+    </div>    
+</body>
+</html>
