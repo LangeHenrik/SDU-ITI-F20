a8df2966a281daa11130100efe408db9574b4d00
Mehmet Øzer <meuze18@student.sdu.dk>
2020-03-07 05:10:17 +0100

    Update Registration.js

--- qanuu18/extfiles/Registration.js
+++ qanuu18/extfiles/Registration.js
@@ -1,9 +1,9 @@
 console.log('javascript is running');
 
-function checkallforms(){
-checkusername();
-checkpassword();
-return false;
+function checkallforms() {
+	checkusername();
+	checkpassword();
+	return false;
 }
 
 //Username
@@ -12,18 +12,18 @@ let usernameInput = document.getElementById("username");
 let usernameInfo = document.getElementById("usernameinfo");
 usernameInput.addEventListener('keyup', checkusername);
 
-function checkusername(){
+function checkusername() {
 
-if(usernameRegEx.test(usernameInput.value)){
-console.log('Valid Username entered');
-usernameInput.style.borderColor = "green";
-usernameInfo.innerText = "";
-return true;
-} else {
-console.log('Invalid Username entered');
-usernameInput.style.borderColor = "red";
-usernameInfo.innerText = "Username Invalid Must be 4-13 letters.";
-}
+	if (usernameRegEx.test(usernameInput.value)) {
+		console.log('Valid Username entered');
+		usernameInput.style.borderColor = "green";
+		usernameInfo.innerText = "";
+		return true;
+	} else {
+		console.log('Invalid Username entered');
+		usernameInput.style.borderColor = "red";
+		usernameInfo.innerText = "Username Invalid Must be 4-13 letters.";
+	}
 }
 
 
@@ -34,18 +34,16 @@ let passwordInput = document.getElementById("password");
 let passwordInfo = document.getElementById("passwordinfo");
 passwordInput.addEventListener('keyup', checkpassword);
 
-function checkpassword(){
+function checkpassword() {
 
-if(passwordRegEx.test(passwordInput.value)){
-console.log('Valid Password entered');
-passwordInput.style.borderColor = "green";
-passwordInfo.innerText = "";
-return true;
-} else {
-console.log('Invalid Password entered');
-passwordInput.style.borderColor = "red";
-passwordInfo.innerText = "Password Invalid Must contain 6 characters with uppercase,lowercase letters. numbers and special characters ex ( @,$,€)";
+	if (passwordRegEx.test(passwordInput.value)) {
+		console.log('Valid Password entered');
+		passwordInput.style.borderColor = "green";
+		passwordInfo.innerText = "";
+		return true;
+	} else {
+		console.log('Invalid Password entered');
+		passwordInput.style.borderColor = "red";
+		passwordInfo.innerText = "Password Invalid Must contain 6 characters with uppercase,lowercase letters. numbers and special characters ex ( @,$,€)";
+	}
 }
-}
-
-
