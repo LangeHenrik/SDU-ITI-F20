5cfb198a9a54ca5ae6ced5a4be089849b90e05c4
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-03-08 02:07:49 +0100

    upload style

--- MSchr18-Phans18-Mach018/Include/upload.js
+++ MSchr18-Phans18-Mach018/Include/upload.js
@@ -1,23 +1,20 @@
 var picupload = document.getElementById('picupload');
-var container = document.getElementById("imgUploadContain");
+var container = document.getElementById("preview");
 container.overflow = scroll;
 
-function addImg(imgFileNr, files) 
+function addImg(imgFileNr, files)
 {
 console.log(imgFileNr);
 console.log(files);
 
-  if (imgFileNr > 0) 
+  if (imgFileNr > 0)
   {
     var img = document.createElement("img");
     img.src = URL.createObjectURL(files[imgFileNr-1]);
-    img.style = "max-height: 90%; max-width: 100%";
 
     var imgText = document.createElement("p");
     imgText.innerHTML = "Name: " + files[imgFileNr-1].name;
-    imgText.style = "background-color: grey; margin: 0 0 5px 0; padding: 10px 0;" 
-                  + "width: 100%; text-align: center; word-wrap: break-word;";
-    
+
     container.appendChild(img);
     container.appendChild(imgText);
 
@@ -25,15 +22,15 @@ console.log(files);
   }
 }
 
-picupload.onchange = function() 
+picupload.onchange = function()
 {
   // Cleaning out children.
   while (container.children["length"] > 0) {
     container.removeChild(container.children[0]);
   }
-  
+
   // Is any of the files bad?
-  for (var i = 0; i < this.files["length"]; i++) 
+  for (var i = 0; i < this.files["length"]; i++)
   {
     if (!this.files[i].type.match("image.*"))
     {
@@ -42,7 +39,7 @@ picupload.onchange = function()
       return;
     }
   }
-  
+
   // Adding new images to container.
   addImg(this.files["length"], this.files);
 }
--- Mschr18-Phans18-Mach018/upload.php
+++ Mschr18-Phans18-Mach018/upload.php
@@ -3,25 +3,30 @@
   include_once('authendicate.php');
 ?>
     <section id="content">
-        <div class="upload" id="upload">
-          <h1 id="title">Upload at </h1> <?php include_once('Include/chalkbord.php') ?>
-              <form method="post" action="uploadImages.php" enctype='multipart/form-data'>
-                <label for="picupload" style="margin: 5px;">Select a file:</label><br>
-                <input type="file" name="picupload[]"  id="picupload" multiple accept="image/*" required>
-                <input type="submit" name="submitpic" id="submitpic" value="Upload">
-                <label style="margin-right: 20px; float: right">My Images</label>
-              </form>
-              <div>
-                  <div id="imgUploadContain" name="imgUploadContain" style="width: 49.4%; overflow: auto; 
-                  display: inline-block; height: 80%; border: grey dashed 5px;"></div>
-                  <div id="imgContain" name="imgContain" style="width: 49.4%; overflow: auto; float: right;
-                  display: inline-block; height: 80%; border: grey dashed 5px; margin-left: -20px;">
-                    <?php 
-                      include_once('include/uploadRetrieveImgs.php');
-                    ?>
-                  </div>
-              </div>
+      <h1 id="title">Upload to </h1> <?php include_once('Include/chalkbord.php') ?>
+      <div class="upload" id="upload">
+
+        
+        <div class="select" id="select">
+          <h4>Select a file...</h4>
+          <form method="post" action="uploadImages.php" enctype='multipart/form-data'>
+            <input type="file" name="picupload[]"  id="picupload" multiple accept="image/*" required>
+            <input type="text" name="" value="" placeholder="Titel" style="width:300px;">
+            <textarea name="name" rows="8" cols="80" style="resize: none; width: 300px; height: 100px;"></textarea>
+            <input type="submit" name="submitpic" id="submitpic" value="Upload">
+          </form>
+
+          <div id="preview" name="preview"></div>
         </div>
+        <div class="myuploads" id="myuploads">
+          <h4>My uploads.</h4>
+          <?php
+            include_once('include/uploadRetrieveImgs.php');
+          ?>
+        </div>
+
+
+      </div>
     </section>
 
     <script src="Include/upload.js"></script>
