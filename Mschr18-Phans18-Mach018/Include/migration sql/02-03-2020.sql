b63f90e979ef316379ceaad769fb7596e4475e1b
Mharti-Mudcrab <Mharti-Mudcrab@users.noreply.github.com>
2020-03-09 12:29:51 +0100

    Update 02-03-2020.sql

--- Mschr18-Phans18-Mach018/Include/migration sql/02-03-2020.sql
+++ Mschr18-Phans18-Mach018/Include/migration sql/02-03-2020.sql
@@ -53,6 +53,11 @@ ADD COLUMN `titel` VARCHAR(50) NOT NULL AFTER `username`;
 ALTER TABLE `iti`.`picture` 
 ADD COLUMN `description` VARCHAR(300) NOT NULL AFTER `titel`;
 
+# 09-03-2020
+-- Adding admin user with username: "admin", and password: "Administrator1!"
+INSERT INTO users (username, passw, fullname, phone, email, signup) VALUES
+('admin', '$2y$10$bl48UKL80MWqPTZxu7Fc6OOclpCz/X1gf6ZZa/5/Sf5YGC3kDbOJm', 'Mr. Administrator', '+1234567890', 'admin@mail.dk', NOW());
+
 -- Vi havde problem med at forbinde til databsen indtil vi kørte følgende
 # ALTER USER 'root'@'localhost' IDENTIFIED WITH mysql_native_password BY 'password'; 
 -- Hvor password er det password du vil bruge til db'en.
