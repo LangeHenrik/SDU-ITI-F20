246ad17344da18893745e36308ca8660fef86e21
Jakob-k <jakob.kaae1@gmail.com>
2020-03-09 13:57:56 +0100

    Fixed showing and hiding content via nav bar

--- jakaa18/Assignment1/scripts/scripts.js
+++ jakaa18/Assignment1/scripts/scripts.js
@@ -3,37 +3,21 @@ if (document.getElementById("register")) {
         document.getElementById("registerView").style.display = "block";
         document.getElementById("loginView").style.display = "none";
     })
-} else if (document.getElementById("homelink")) {
-    document.getElementById("homelink").addEventListener("click", function () {
-        document.getElementById("homelink").style.display = "block";
-        document.getElementById("photolink").style.display = "none";
-        document.getElementById("userslink").style.display = "none";
-        document.getElementById("fileId").style.display = "none";
-        document.getElementById("header").style.display = "none";
-        document.getElementById("descriptionId").style.display = "none";
-        document.getElementById("imgSubmit").style.display = "none";
+}
+function showHomeLink() {
+    document.getElementById("uploadForm").style.display = "none";
+    document.getElementById("imagePage").style.display = "block";
+}
 
-    })
-} else if (document.getElementById("photolink")) {
-    document.getElementById("photolink").addEventListener("click", function () {
-        document.getElementById("homelink").style.display = "none";
-        document.getElementById("photolink").style.display = "block";
-        document.getElementById("userslink").style.display = "none";
-        document.getElementById("fileId").style.display = "block";
-        document.getElementById("header").style.display = "block";
-        document.getElementById("descriptionId").style.display = "block";
-        document.getElementById("imgSubmit").style.display = "block";
-    })
-} else if (document.getElementById("userslink")) {
-    document.getElementById("userslink").addEventListener("click", function () {
-        document.getElementById("homelink").style.display = "none";
-        document.getElementById("photolink").style.display = "none";
-        document.getElementById("userslink").style.display = "block";
-        document.getElementById("fileId").style.display = "none";
-        document.getElementById("header").style.display = "none";
-        document.getElementById("descriptionId").style.display = "none";
-        document.getElementById("imgSubmit").style.display = "none";
-    })
+function showPhotoLink ()
+{
+    document.getElementById("uploadForm").style.display = "block";
+    document.getElementById("imagePage").style.display = "none";
+
+}
+function showUsersLink() {
+    document.getElementById("uploadForm").style.display = "none";
+    document.getElementById("imagePage").style.display = "none";
 }
 
 
