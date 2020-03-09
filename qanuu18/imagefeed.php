0edf2169b73a879d9ed5683b747cb320f74f37d5
Mehmet Øzer <meuze18@student.sdu.dk>
2020-03-09 03:32:12 +0100

    hurtig

--- qanuu18/imagefeed.php
+++ qanuu18/imagefeed.php
@@ -8,19 +8,20 @@
 
 <body>
     <form class="userlist" name="userlist" method="GET" action="userlist.php">
-        
-    
-    <p>
-            <input type="submit" name="userlist" id="ulbtn" value="Userlist">
+        <p>
+        <input type="submit" onclick="loadDoc()" name="userlist" id="ulbtn" value="Userlist">
+        <p class="ulbtn" id=ulbtn></p>
         </p>
     </form>
-
-
 
     <form class="uploadImage" name="uploadImage" method="POST" action="uploadimage.php">
         <p>
         <input type="submit" name="uploadImage" id="uloadbtn" value="Upload Picture">
-</p>
-</form>
+        </p>
+    </form>
+
+    <script src="extfiles/userlist.js "></script>
+
 
 </body>
+</html>
