c6d483fa5f3e54157c673f4719ad1048dd4c20fe
rinyang2 <49273615+rinyang2@users.noreply.github.com>
2020-03-09 11:18:24 +0100

    Password Strength
    implemented password strength check code

--- signup.php
+++ signup.php
@@ -2,6 +2,8 @@
     require "header.php";
 ?>
         <script src="./js/regex.js"></script>
+        <script src="http://code.jquery.com/jquery-1.11.1.min.js"></script>
+        <script src="./js/passwd_str.js"></script>
         <main>
           <div class="wrapper-main">
             <div id="formContent">
@@ -9,7 +11,8 @@
               <form onKeyUp="return validateFields()" class="form-signup" method="post">
                 <input type="text" name="username" id="username" placeholder="Username" autofocus require>
                 <input type="text" name="email-register" id="email-register" placeholder="E-mail" require>
-                <input type="password" name="pwd-register" id="pwd-register" placeholder="Password" require>
+                <input type="password" name="pwd-register" id="pwd-register"  onKeyUp="checkPasswordStrength();" placeholder="Password" require>
+                <div id="password-strength-status"></div>
                 <input type="password" name="pwd-repeat" id="pwd-repeat" placeholder="Repeat Password" require>
                 <button type="submit" name="signup-submit">Sign-up</button>
               </form>
