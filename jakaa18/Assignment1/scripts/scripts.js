316297ac258fc9cb86c4ddb8b3e23e8103148f08
Jakob-k <jakob.kaae1@gmail.com>
2020-03-09 12:20:23 +0100

    Updated if for javaScript

--- jakaa18/Assignment1/scripts/scripts.js
+++ jakaa18/Assignment1/scripts/scripts.js
@@ -1,37 +1,40 @@
-document.getElementById("register").addEventListener("click",function () {
-    document.getElementById("registerView").style.display = "block";
-    document.getElementById("loginView").style.display = "none";
-})
-document.getElementById("homelink").addEventListener("click",function () {
-    document.getElementById("homelink").style.display = "block";
-    document.getElementById("photolink").style.display = "none";
-    document.getElementById("userslink").style.display = "none";
-    document.getElementById("fileId").style.display = "none";
-    document.getElementById("header").style.display = "none";
-    document.getElementById("descriptionId").style.display = "none";
-    document.getElementById("imgSubmit").style.display = "none";
+if (document.getElementById("register")) {
+    document.getElementById("register").addEventListener("click",function () {
+        document.getElementById("registerView").style.display = "block";
+        document.getElementById("loginView").style.display = "none";
+    })
+} else if (document.getElementById("homelink")) {
+    document.getElementById("homelink").addEventListener("click", function () {
+        document.getElementById("homelink").style.display = "block";
+        document.getElementById("photolink").style.display = "none";
+        document.getElementById("userslink").style.display = "none";
+        document.getElementById("fileId").style.display = "none";
+        document.getElementById("header").style.display = "none";
+        document.getElementById("descriptionId").style.display = "none";
+        document.getElementById("imgSubmit").style.display = "none";
 
-})
-document.getElementById("photolink").addEventListener("click",function () {
-    document.getElementById("homelink").style.display = "none";
-    document.getElementById("photolink").style.display = "block";
-    document.getElementById("userslink").style.display = "none";
-    document.getElementById("fileId").style.display = "block";
-    document.getElementById("header").style.display = "block";
-    document.getElementById("descriptionId").style.display = "block";
-    document.getElementById("imgSubmit").style.display = "block";
-
-})
-document.getElementById("userslink").addEventListener("click",function () {
-    document.getElementById("homelink").style.display = "none";
-    document.getElementById("photolink").style.display = "none";
-    document.getElementById("userslink").style.display = "block";
-    document.getElementById("fileId").style.display = "none";
-    document.getElementById("header").style.display = "none";
-    document.getElementById("descriptionId").style.display = "none";
-    document.getElementById("imgSubmit").style.display = "none";
-
-})
+    })
+} else if (document.getElementById("photolink")) {
+    document.getElementById("photolink").addEventListener("click", function () {
+        document.getElementById("homelink").style.display = "none";
+        document.getElementById("photolink").style.display = "block";
+        document.getElementById("userslink").style.display = "none";
+        document.getElementById("fileId").style.display = "block";
+        document.getElementById("header").style.display = "block";
+        document.getElementById("descriptionId").style.display = "block";
+        document.getElementById("imgSubmit").style.display = "block";
+    })
+} else if (document.getElementById("userslink")) {
+    document.getElementById("userslink").addEventListener("click", function () {
+        document.getElementById("homelink").style.display = "none";
+        document.getElementById("photolink").style.display = "none";
+        document.getElementById("userslink").style.display = "block";
+        document.getElementById("fileId").style.display = "none";
+        document.getElementById("header").style.display = "none";
+        document.getElementById("descriptionId").style.display = "none";
+        document.getElementById("imgSubmit").style.display = "none";
+    })
+}
 
 
 function checkLogin() {
