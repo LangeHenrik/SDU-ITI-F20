f3123a46ef3df522c87505c439ef6bb6a7614389
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-03-09 11:53:03 +0100

    get image style update

--- Mschr18-Phans18-Mach018/Include/PDO/getUploadedImages.php
+++ Mschr18-Phans18-Mach018/Include/PDO/getUploadedImages.php
@@ -11,7 +11,7 @@
     $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
     if (isset($_POST["myuploads"]) && $_POST["myuploads"]) {
-      $stmt = $conn->prepare("SELECT titel, imagebase64, descripton, uploaddate FROM picture WHERE username = :username");
+      $stmt = $conn->prepare("SELECT titel, imagebase64, description, uploaddate FROM picture WHERE username = :username");
       $stmt->bindParam(':username', $_SESSION['username']);
       $stmt->execute();
       $stmt->setFetchMode(PDO::FETCH_NUM);
@@ -20,16 +20,27 @@
       {
         ?>
           <div class="picturecontainer">
-            <p class="titel"><?php echo "Titel: " . $imagebase64Pics[$i][0] ?></p>
-            <p class="byuser"><?php echo "By: " . $_SESSION['username'] ?></p>
-            <img src= <?php echo $imagebase64Pics[$i][1] ?> alt= <?php echo $_SESSION['username'] . ":". $imagebase64Pics[$i][0] ?> >
-            <p class="descripton"><?php echo "Descripton: <br>" . $imagebase64Pics[$i][3] ?></p>
-            <p class="date"><?php echo "Date: " . $imagebase64Pics[$i][3] ?></p>
+            <table>
+              <tr>
+                <td class="titel"><?php echo "Titel: " . $imagebase64Pics[$i][0] ?></td>
+                <td class="byuser"><?php echo "By: " . $_SESSION['username'] ?></td>
+              </tr>
+              <tr>
+                <td>
+                  <img src= <?php echo $imagebase64Pics[$i][1] ?> alt= <?php echo $_SESSION['username'] . ":". $imagebase64Pics[$i][0] ?> >
+                </td>
+                <td class="descripton"><?php echo "Descripton: <br>" . $imagebase64Pics[$i][2] ?></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td class="date"><?php echo "Date: " . $imagebase64Pics[$i][3] ?></td>
+              </tr>
+            </table>
           </div>
         <?php
       }
     } else {
-      $stmtString = "SELECT titel, username, imagebase64, descripton, uploaddate FROM picture";
+      $stmtString = "SELECT titel, username, imagebase64, description, uploaddate FROM picture";
       $stmt = $conn->prepare($stmtString);
       $stmt->execute();
       $stmt->setFetchMode(PDO::FETCH_NUM);
@@ -38,11 +49,22 @@
       {
         ?>
           <div class="picturecontainer">
-            <p class="titel"><?php echo "Titel: " . $imagebase64Pics[$i][0] ?></p>
-            <p class="byuser"><?php echo "By: " . $imagebase64Pics[$i][1] ?></p>
-            <img src= <?php echo $imagebase64Pics[$i][2] ?> alt= <?php echo $imagebase64Pics[$i][1] . ":". $imagebase64Pics[$i][0] ?> >
-            <p class="descripton"><?php echo "Descripton: <br>" . $imagebase64Pics[$i][3] ?></p>
-            <p class="date"><?php echo "Date: " . $imagebase64Pics[$i][4] ?></p>
+            <table>
+              <tr>
+                <td class="titel"><?php echo "Titel: " . $imagebase64Pics[$i][0] ?></td>
+                <td class="byuser"><?php echo "By: " . $imagebase64Pics[$i][1] ?></td>
+              </tr>
+              <tr>
+                <td>
+                  <img src= <?php echo $imagebase64Pics[$i][2] ?> alt= <?php echo $imagebase64Pics[$i][1] . ":". $imagebase64Pics[$i][0] ?> >
+                </td>
+                <td class="descripton"><?php echo "Descripton: <br>" . $imagebase64Pics[$i][3] ?></td>
+              </tr>
+              <tr>
+                <td></td>
+                <td class="date"><?php echo "Date: " . $imagebase64Pics[$i][4] ?></td>
+              </tr>
+            </table>
           </div>
         <?php
       }
