7a6ef378da1a7138595902a72279ab236ed79901
enzobes <enzobes@me.com>
2020-03-26 16:27:55 +0100

    Clean ContactController

--- enbes20-elpoy20-bapio20/mvc/app/controllers/ContactController.php
+++ enbes20-elpoy20-bapio20/mvc/app/controllers/ContactController.php
@@ -19,42 +19,14 @@ class ContactController extends Controller {
 	  if ($q === "") {
 	    $res = $this->model('User')->getAll();
 
-	     //while ($row = $res->fetch()){
-			 $row = $res->fetchAll();
-			 $viewbag['output_userlist'] =  $row;
+			$row = $res->fetchAll();
+		  $viewbag['output_userlist'] =  $row;
 
-			// print($row);
-			//	var_dump($row);
-	    // 		$viewbag['output_userlist'] = $viewbag['output_userlist'] . "
-	    // 		<div class='user_card'>
-	    //       		<ul class=''>
-			//             <li>
-		  //     				<div>Username : ". $row['username'] ."</div>
-		  //     			 	<div>Email : ". $row['email'] ."</div>
-			//             </li>
-	    // 			</ul>
-	    //     	</div>
-	    // 		";
-	   //   }
 	  } else {
 	    $res = $this->model('User')->getAll($q);
 			$row = $res->fetchAll();
 			$viewbag['output_userlist'] =  $row;
 
-
-	    // while ($row = $res->fetch()){
-	    // 		$viewbag['output_userlist'] = $viewbag['output_userlist'] . "
-	    // 		<div class='user_card'>
-		  //         <ul class=''>
-		  //           <li>
-		  //             <div>Username : ". $row['username'] ."</div>
-		  //             <div>Email : ". $row['email'] ."</div>
-		  //           </li>
-    	// 	      </ul>
-	    //         </div>
-	    // 		";
-	    //   }
-	      //die();
 	  }
 	  $res->closeCursor(); // End the request
 	  $this->view('contacts/index', $viewbag);
