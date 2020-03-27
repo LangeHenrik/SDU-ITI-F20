24f321378ad098234d1a1263d739ecd1080b9ccf
Mharti-Mudcrab <Mharti-Mudcrab@users.noreply.github.com>
2020-03-27 11:59:36 +0100

    Vi viser nu at username eller pw er forkert hvis en af dem er + code cleanup

--- Mschr18-Phans18-Mach018/Include/phpUtils/login.php
+++ Mschr18-Phans18-Mach018/Include/phpUtils/login.php
@@ -5,10 +5,6 @@ if (session_status() == PHP_SESSION_NONE) {
 require_once '../db_config.php';
 try
 {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
-
     // Har vi en bruger med samme brugernavn?
     function trim_input(&$input) {
         $input = trim($input);
@@ -24,19 +20,17 @@ try
     $stmt->bindParam(':username', $inputArr["username"]);
     $stmt->execute();
     $stmt->setFetchMode(PDO::FETCH_NUM); // FETCH_NUM -> returnerer array indexeret i colonner angivet i tal.                                         // Andre return methoder er beskrevet her: https://www.php.net/manual/en/pdostatement.fetch.php
-    $result = $stmt->fetchAll();
+    $result = $stmt->fetch();
 
-    $rows = count($result);
-
-    if ($rows < 1) {
-      echo "<script>
-              alert('Username is wrong');
-              window.location= '../../index.php';
-            </script>";
-    } else {
-      $fetchetUsername = $result[0][0];
-      $fetchetHashPasword = $result[0][1];
-      $fetchetFullname = $result[0][2];
+    if (!$result) { ?>
+      <script>
+          alert('Username or Pasword is wrong');      // Username is wrong, but we wont tell
+          window.location= '../../index.php';
+      </script>
+    <?php } else {
+      $fetchetUsername = $result[0];
+      $fetchetHashPasword = $result[1];
+      $fetchetFullname = $result[2];
 
       if (password_verify($inputArr["password"], $fetchetHashPasword)) {
         $_SESSION['logged_in'] = true;
@@ -44,18 +38,18 @@ try
         $_SESSION['username'] = $fetchetUsername;
         header("location:../../feed.php");
       }
-      else {
-        echo "<script>
-                alert('Pasword is wrong');
-                window.location= '../../index.php';
-              </script>";
-      }
+      else { ?>
+        <script>
+            alert('Username or Pasword is wrong');    // Password is wrong, but we wont tell
+            window.location= '../../index.php';
+        </script>
+      <?php }
     }
 }
 catch(PDOException $e)
-{
-    echo "Connection failed: " . $e->getMessage() . ".\n code " . $e->getCode();
-}
+{ ?>
+    Connection failed: <?=$e->getMessage()?> \n code <?=$e->getCode()?>
+<?php } 
 
 $conn = null;
 ?>
