7cd008bae9972aa43c9fd0ae1eebe2ce3fe229d2
Signetp <siped18@student.sdu.dk>
2020-03-30 09:42:25 +0200

    MVC

---
+++ siped18/mvc/app/core/db_config.php
@@ -0,0 +1,10 @@
+
+<?php
+class DB_Config {
+	
+	protected $host = "localhost:3306";
+    protected $uname = "YOUR_SYSTEM_USER";
+    protected $pword = "siped18";
+    protected $dbname = "user";
+
+}
