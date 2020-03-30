90cf0d2fde7a1a0f2a8b29606cfd4ea78862c371
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-03-30 10:04:37 +0200

    opsætning

--- rasmt18_soepe16_matry18/mvc/app/core/db_config.php
+++ rasmt18_soepe16_matry18/mvc/app/core/db_config.php
@@ -4,7 +4,10 @@ class DB_Config {
 	protected $servername = 'localhost';
 	protected $username = 'root';
 	protected $password = 'root';
-	protected $dbname = 'hela';
+	//protected $password = '1234'; //mathias
+	// protected $servername = 'localhost:3307'; //mathias
+	protected $dbname = 'rasmt18_soepe16_matry18';
+
 
 }
 	
