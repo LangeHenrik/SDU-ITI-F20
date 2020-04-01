ad5e3a82347a74b815688b238025a91c284838cf
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-04-01 14:00:46 +0200

    hot fix

--- rasmt18_soepe16_matry18/mvc/app/views/image/index.php
+++ rasmt18_soepe16_matry18/mvc/app/views/image/index.php
@@ -3,6 +3,10 @@
         <h1>Image Feed</h1>
         <h2>Look at all the cool images below</h2>
         <button type="button" id="loadImages" name="loadImages" onclick="return getImage()">Load images!</button>
+        <div id="imageContainer">
+
+        </div>
     </div>
+    
     
 <?php include '../app/views/partials/foot.php'; ?>
