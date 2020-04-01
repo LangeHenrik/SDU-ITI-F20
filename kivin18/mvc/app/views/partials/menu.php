69ae5afd4dd63cfd63ad381394c1ef4ccea4434b
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-04-01 21:18:12 +0200

    Add login form and css file

--- kivin18/mvc/app/views/partials/menu.php
+++ kivin18/mvc/app/views/partials/menu.php
@@ -5,9 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link rel="stylesheet" href="../mvc/public/css/style.css">
     </head>
     <body>
-
+<h1>Image Feed</h1>
 <div style="background-color: lightblue;">Menu partial view</div>
 
 <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']) : ?>
@@ -15,4 +16,4 @@
 <a href="/kivin18/mvc/public/user/logout">log out</a>
 
 <?php endif; ?>
-
+<div class="wrapper">
