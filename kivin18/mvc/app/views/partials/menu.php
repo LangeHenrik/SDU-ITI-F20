5e9bbbfb77ed5d56771e7c079712378b5d869817
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-04-01 21:27:42 +0200

    Update css and js path

--- kivin18/mvc/app/views/partials/menu.php
+++ kivin18/mvc/app/views/partials/menu.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="../mvc/public/css/style.css">
+    <link rel="stylesheet" href="../public/css/style.css">
     </head>
     <body>
 <h1>Image Feed</h1>
