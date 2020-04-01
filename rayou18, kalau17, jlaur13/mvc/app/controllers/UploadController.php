00a365ae8978fa63af85cb458ec1b5864b21a14e
Kasper Ejby <kasper.ejby@gmail.com>
2020-04-01 13:02:36 +0200

    added form in upload view

--- rayou18, kalau17, jlaur13/mvc/app/controllers/UploadController.php
+++ rayou18, kalau17, jlaur13/mvc/app/controllers/UploadController.php
@@ -3,4 +3,13 @@
 class UploadController extends Controller {
 
 
+    public function uploadPictures($param1 , $param2 , $param3 , $param4){
+        $picture = $this->model('Picture');
+        $name = $param1;
+        $header = $param2;
+        $description = $param3;
+        $file = $param4;
+        $picture->upload($name , $header , $description , $file);
+    }
+
 }
