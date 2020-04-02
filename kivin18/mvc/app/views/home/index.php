75fc6c3607edf74c0ebc5e7e3ebb63ec15d175e5
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-04-02 10:14:30 +0200

    Update login form css and action and add register hyperlink

--- kivin18/mvc/app/views/home/index.php
+++ kivin18/mvc/app/views/home/index.php
@@ -1,6 +1,6 @@
 <?php include '../app/views/partials/menu.php'; ?>
 
-    <form>
+    <form method="post" action="/kivin18/mvc/public/home/login">
         <div class="form-group">
             <label for="username">Email address</label>
             <input type="text" class="form-control" id="username" name="username" placeholder="Enter username">
@@ -10,7 +10,7 @@
             <label for="password">Password</label>
             <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
         </div>
-        <button type="submit" class="btn btn-primary">Submit</button>
+        <button type="submit" class="btn btn-primary">Login</button>
+        <a class="btn btn-primary" href="/kivin18/mvc/public/user/logout">Register</a>
     </form>
-
 <?php include '../app/views/partials/foot.php'; ?>
