15f32ea6c316435d5d90c524a1cfe8c848d8c94c
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-04-02 12:46:05 +0200

    Bootstrap implement

--- Mschr18-Phans18-Mach018/mvc/app/views/partials/header.php
+++ Mschr18-Phans18-Mach018/mvc/app/views/partials/header.php
@@ -6,13 +6,21 @@
 <!DOCTYPE html><html>
   <head>
     <title>ChalkBoard</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <!--stylesheet-->
-    <link rel="stylesheet" type="text/css" href="../../app/views/include/style.css">
+
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!--Fontawsome-->
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
-    <!--reuse loginnav-->
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+    <!-- OBS Bootstrap Style in fotter -->
+
+    <!-- lokal stylesheet-->
+    <link rel="stylesheet" type="text/css" href="../app/views/include/style.css">
+
   </head>
   <!--Comment-->
   <body>
--- Mschr18-Phans18-Mach018/mvc/app/views/partials/navLogin.php
+++ Mschr18-Phans18-Mach018/mvc/app/views/partials/navLogin.php
@@ -1,8 +1,8 @@
 
 <nav class="loginnav" id="loginnav">
   <ul>
-    <li><a href="index.php">Frontpage <i class="fas fa-chalkboard"></i></a></li>
-    <li><a href="registration.php">Sign up <i class="fas fa-user-plus"></i></a></li>
+    <li><a href="Home">Frontpage <i class="fas fa-chalkboard"></i></a></li>
+    <li><a href="Registration">Sign up <i class="fas fa-user-plus"></i></a></li>
     <li>
       <?php
         include('loginform.php');
