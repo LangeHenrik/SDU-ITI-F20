8499326c318ae218db0203e1667d384f4126c99e
Mharti-Mudcrab <Mharti-Mudcrab@users.noreply.github.com>
2020-04-04 16:08:21 +0200

    Update navbar.php

--- Mschr18-Phans18-Mach018/mvc/app/views/partials/navbar.php
+++ Mschr18-Phans18-Mach018/mvc/app/views/partials/navbar.php
@@ -1,5 +1,5 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-  <a class="navbar-brand" href="/Mschr18-Phans18-Mach018/mvc/public/Home/Index">
+  <a class="navbar-brand" href="<?=BASE_URL?>Home/Index">
     <?php include('../app/views/partials/chalkbordlogo.php') ?>
   </a>
 
@@ -19,25 +19,25 @@
   <div class="collapse navbar-collapse" id="navbarNav">
     <ul class="navbar-nav " >
       <li class="nav-item">
-        <a class="nav-link" href="/Mschr18-Phans18-Mach018/mvc/public/Home/Index">Home <i class="fas fa-home"></i></a>
+        <a class="nav-link" href="<?=BASE_URL?>Home/Index">Home <i class="fas fa-home"></i></a>
       </li>
 
       <?php if( isset($_SESSION['logged_in']) && ($_SESSION['logged_in']) ) { ?>
 
           <li class="nav-item">
-            <a class="nav-link"  href="/Mschr18-Phans18-Mach018/mvc/public/Home/Feed">ChalkBoard-Feed <i class="fas fa-comment-alt"></i></a>
+            <a class="nav-link"  href="<?=BASE_URL?>Home/Feed">ChalkBoard-Feed <i class="fas fa-comment-alt"></i></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link"  href="/Mschr18-Phans18-Mach018/mvc/public/Home/Upload">Upload <i class="fas fa-file-upload"></i></i></a>
+            <a class="nav-link"  href="<?=BASE_URL?>Home/Upload">Upload <i class="fas fa-file-upload"></i></i></a>
           </li>
           <li class="nav-item">
-            <a class="nav-link"  href="/Mschr18-Phans18-Mach018/mvc/public/Home/Users">Users <i class="fas fa-address-book"></i></i></a>
+            <a class="nav-link"  href="<?=BASE_URL?>Home/Users">Users <i class="fas fa-address-book"></i></i></a>
           </li>
 
       <?php } else { ?>
 
         <li class="nav-item">
-          <a class="nav-link"  href="/Mschr18-Phans18-Mach018/mvc/public/Home/Registration">Sign up <i class="fas fa-user-plus"></i></a>
+          <a class="nav-link"  href="<?=BASE_URL?>Home/Registration">Sign up <i class="fas fa-user-plus"></i></a>
         </li>
 
       <?php } ?>
