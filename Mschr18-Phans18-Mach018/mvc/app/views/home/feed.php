cec87869b0e085ff9fdd35bc4f80a9a58cda7e29
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-04-06 23:29:14 +0200

    feed design

--- Mschr18-Phans18-Mach018/mvc/app/views/home/feed.php
+++ Mschr18-Phans18-Mach018/mvc/app/views/home/feed.php
@@ -1,39 +1,36 @@
 <?php
-  include_once('../app/views/partials/header.php');
-?>
-    <section id="content">
-      <h1 id="title">Feed </h1> <?php include_once('../app/views/partials/chalkbordlogo.php') ?>
-      <div class="feed" id="feed">
-        <?php
-        if (!empty($viewbag)) {
-          for ($i=0; $i < count($viewbag); $i++) {
-            ?>
-              <div class="picturecontainer" id="<?=$i?>">
-                  <table>
-                      <tr>
-                          <td class="titel" width="200">Titel: <?=$viewbag[$i][0]?></td>
-                          <td class="byuser" width="240">By: <?=$viewbag[$i][1]?></td>
-                      </tr>
-                      <tr>
-                          <td height="170">
-                              <img src= <?=$viewbag[$i][2]?> alt="Picture by <?=$viewbag[$i][1]?>, titeled <?=$viewbag[$i][0]?>" >
-                          </td>
-                          <td class="descripton">Descripton:<br><?=$viewbag[$i][3]?></td>
-                      </tr>
-                      <tr>
-                          <td></td>
-                          <td class="date">Date: <?=$viewbag[$i][4]?></td>
-                      </tr>
-                  </table> 
-              </div>
-            <?php
-          }
-        }
-        ?>
-        
-      </div>
-    </section>
+  $_SESSION['page_titel'] .= " | Feed";
+  $_SESSION['page_header::before'] = "";
+  $_SESSION['page_header::after'] = " Feed.";
+  $_SESSION['page_sub_header'] = "Follow image feed for updates whenever new stories are uploaded.";
 
-<?php
+  include_once('../app/views/partials/header.php');
+
+  if (!empty($viewbag)) {
+    for ($i=0; $i < count($viewbag); $i++) {
+      ?>
+      <div class="card" style="width: 18rem;" id="card_<?=$i?>">
+        <img class="card-img-top" src=<?=$viewbag[$i][2]?> alt="Card image cap">
+        <div class="card-body">
+          <h5 class="card-title"><?=$viewbag[$i][0]?></h5>
+          <p class="card-text"><?=$viewbag[$i][3]?>.</p>
+        </div>
+        <div class="card-footer">
+          <div class="row">
+            <div class="col">
+              <small class="float-left text-muted"><?=$viewbag[$i][1]?></small>
+            </div>
+            <div class="col">
+              <small class="float-right text-muted"><?=$viewbag[$i][4]?></small>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
+      <?php
+    }
+  }
+  
  include_once('../app/views/partials/footer.php');
 ?>
