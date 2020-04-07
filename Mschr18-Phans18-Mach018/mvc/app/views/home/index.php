71f166ae11307c6781ccf41e52dac0546cb7762c
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-04-07 13:11:40 +0200

    home pahe login

--- Mschr18-Phans18-Mach018/mvc/app/views/home/index.php
+++ Mschr18-Phans18-Mach018/mvc/app/views/home/index.php
@@ -14,10 +14,60 @@
   include_once('../app/views/partials/header.php');
 
 
-
-
   if( isset($_SESSION['logged_in']) && ($_SESSION['logged_in']) ) {
+    ?>
 
+<div class="row">
+   <div class="col-sm">
+    <div class="container">
+      <div class="myuploads" id="myuploads">
+        <h4>My uploads.</h4>
+        <?php
+          if (!empty($viewbag)) {
+            for ($i=0; $i < count($viewbag); $i++) {
+              ?>
+                <div class="picturecontainer" id="<?=$i?>">
+                    <table>
+                        <tr>
+                            <td class="titel" width="200">Titel: <?=$viewbag[$i][0]?></td>
+                            <td class="byuser" width="240">By: <?=$viewbag[$i][1]?></td>
+                        </tr>
+                        <tr>
+                            <td height="170">
+                                <img src= <?=$viewbag[$i][2]?> alt="Picture by <?=$viewbag[$i][1]?>, titeled <?=$viewbag[$i][0]?>" >
+                            </td>
+                            <td class="descripton">Descripton:<br><?=$viewbag[$i][3]?></td>
+                        </tr>
+                        <tr>
+                            <td></td>
+                            <td class="date">Date: <?=$viewbag[$i][4]?></td>
+                        </tr>
+                    </table>
+                      <form action="include/PDO/deleteImage.php" method="POST">
+                          <input type="hidden" name="deletebtn" value="<?=$viewbag[$i][5]?>"/>
+                      </form>
+                      <a  class="deleteBtn" href="#" onclick="this.parentNode.children[1].submit()">
+                          <div class="X1"></div>
+                          <div class="X2"></div>
+                      </a>
+                </div>
+              <?php
+            }
+          }
+        ?>
+      </div>
+    </div>
+   </div>
+   <div class="col-sm">
+     <div class="page container-fluid custom-container">
+       <div class="container">
+      api stuffe like wther
+        </div>
+     </div>
+   </div>
+ </div>
+
+    <?php
   } else {
     ?>
 
@@ -68,10 +118,6 @@
       </div>
     </div>
 </div>
-
-
-    </div>
-
 
     <?php
   }
