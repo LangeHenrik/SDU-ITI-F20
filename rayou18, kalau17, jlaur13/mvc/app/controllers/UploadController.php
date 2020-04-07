f902348df8c885250d4b2402eacbe8a97517735c
Kasper Ejby <kasper.ejby@gmail.com>
2020-04-07 10:03:06 +0200

    update

--- rayou18, kalau17, jlaur13/mvc/app/controllers/UploadController.php
+++ rayou18, kalau17, jlaur13/mvc/app/controllers/UploadController.php
@@ -9,11 +9,17 @@ class UploadController extends Controller {
 
 
     public function uploadPictures(){
-        $name = 'kasper';
-        $header = $_POST['header'];
-        $description = $_POST['description'];
-        $file = $_POST['file'];
-        $picture = $this->model('Picture')->upload($name , $header , $description , $file);
+
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+            $name = "kasper"; //filter_var($_POST["username"],FILTER_SANITIZE_STRING);
+            $header = filter_var($_POST["header"],FILTER_SANITIZE_STRING);
+            $description = filter_var($_POST["description"],FILTER_SANITIZE_STRING);
+            $file = $_POST["file"];
+
+            $picture = $this->model('Picture')->upload($name , $header , $description , $file);
+            $this->view('partials/signup_form');
+
+        }
     }
 
 }
