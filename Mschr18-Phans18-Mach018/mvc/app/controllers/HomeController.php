9825bfe7de84b8b4b3b2a475e56fc0b2c225cd48
Mharti-Mudcrab <Mharti-Mudcrab@users.noreply.github.com>
2020-04-07 13:29:03 +0200

    Update HomeController.php

--- Mschr18-Phans18-Mach018/mvc/app/controllers/HomeController.php
+++ Mschr18-Phans18-Mach018/mvc/app/controllers/HomeController.php
@@ -5,7 +5,8 @@ class HomeController extends Controller {
 	// Main / home / default - page.
 	public function index () {
 		if (isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
-			$viewbag = $this->model('Picture')->getPictures($_SESSION['username']);
+			$picture = $this->model('Picture');
+			$viewbag = $picture->getPictures($_SESSION['username']);
 			$this->view('home/index', $viewbag);
 		}
 		else {
