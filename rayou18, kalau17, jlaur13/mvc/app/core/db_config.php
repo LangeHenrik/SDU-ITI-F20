74ca2df06bea5778abcd8ee1132130b0ff8c960b
Symaro <37690011+Symaro@users.noreply.github.com>
2020-04-07 12:30:41 +0200

    Update db_config.php

--- rayou18, kalau17, jlaur13/mvc/app/core/db_config.php
+++ rayou18, kalau17, jlaur13/mvc/app/core/db_config.php
@@ -2,8 +2,8 @@
 class DB_Config {
 	
 	protected $servername = 'localhost';
-	protected $username = 'php';
-	protected $password = '1234';
+	protected $username = 'standard';
+	protected $password = 'password';
 	protected $dbname = 'rayou18_kalau17_jlaur13_assignment2';
 
 }
