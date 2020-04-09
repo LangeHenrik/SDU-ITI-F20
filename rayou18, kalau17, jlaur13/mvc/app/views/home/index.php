2a2d2f528eb93b28e43f6b4677929d34527292c6
Symaro <37690011+Symaro@users.noreply.github.com>
2020-04-09 13:03:02 +0200

    Update index.php

--- rayou18, kalau17, jlaur13/mvc/app/views/home/index.php
+++ rayou18, kalau17, jlaur13/mvc/app/views/home/index.php
@@ -5,7 +5,7 @@
     <meta name="viewport"
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>NetJohnPany</title>
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
           integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
           crossorigin="anonymous">
