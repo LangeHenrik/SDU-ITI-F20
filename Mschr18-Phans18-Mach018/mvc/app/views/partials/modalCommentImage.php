183a42a6da8f31b548b2b262e8175b8686371964
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-04-09 22:09:31 +0200

    modal redy for comment feed

--- Mschr18-Phans18-Mach018/mvc/app/views/partials/modalCommentImage.php
+++ Mschr18-Phans18-Mach018/mvc/app/views/partials/modalCommentImage.php
@@ -1,19 +1,56 @@
-<div class="modal fade" id="modalCommentImage" tabindex="-1" role="dialog" aria-labelledby="modalCommentImage" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
-          <span aria-hidden="true">&times;</span>
-        </button>
+<div class="modal comment fade" id="modalCommentImage" tabindex="-1" role="dialog" aria-labelledby="modalCommentImage" aria-hidden="true">
+  <div class="row modal-image-comment h-100">
+    <div class="col-md-6 col-card my-auto" >
+
+
+      <div class="modal-dialog" role="document">
+
+
+        <div class="card" style="width: 18rem;">
+          <img class="card-img-top" src="/Mschr18-Phans18-Mach018/mvc/public/svg/sduLogo.svg" alt="Brokken image!">
+          <div class="card-body">
+            <h5 class="card-title">This is a test titel</h5>
+            <p class="card-text">This is youst some random texst for the test.</p>
+          </div>
+          <div class="card-footer">
+            <div class="row">
+              <div class="col">
+                <small class="float-left text-muted"><?=$_SESSION['username']?></small>
+              </div>
+              <div class="col">
+                <small class="float-right text-muted"><?=date('Y-m-d')?></small>
+              </div>
+            </div>
+          </div>
+        </div>
+
       </div>
-      <div class="modal-body">
-        ...
-      </div>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
-      </div>
+
+    </div>
+    <div class="col-md-6 col-comment">
+
+
+
+
+
+          <div class="modal-header">
+            <h5 class="modal-title" id="exampleModalLabel">Comment image</h5>
+            <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+              <span aria-hidden="true">&times;</span>
+            </button>
+          </div>
+          <div class="modal-body comment ">
+
+
+            chat goes her
+
+
+          </div>
+
+
+
+
     </div>
   </div>
+
 </div>
