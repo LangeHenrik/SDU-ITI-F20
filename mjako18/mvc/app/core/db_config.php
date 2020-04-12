2f303680f5f444118c6940d7a652731d97e729a1
Unknown <mjako18@student.sdu.dk>
2020-04-12 16:39:42 +0200

    MVC added

---
+++ mjako18/mvc/app/core/db_config.php
@@ -0,0 +1,9 @@
+<?php
+class DB_Config {
+
+	protected $servername = 'localhost';
+	protected $username = 'mjako18';
+	protected $password = 'ITIassignment';
+	protected $dbname = 'image_share';
+
+}
