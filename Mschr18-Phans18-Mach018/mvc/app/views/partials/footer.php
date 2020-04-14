5b3ed3dc5227bab96251ce1c6b3a43ef66e8294f
PeterBuchHansen <60644097+PeterBuchHansen@users.noreply.github.com>
2020-04-14 17:14:58 +0200

    footer script

--- Mschr18-Phans18-Mach018/mvc/app/views/partials/footer.php
+++ Mschr18-Phans18-Mach018/mvc/app/views/partials/footer.php
@@ -34,47 +34,42 @@
         </span>
       </nav>
     </footer>
-  <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-  <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-  <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-  <?php // Loading script for commentsection in feed  
-  if(substr($_SERVER['REQUEST_URI'], -4) === 'feed') { ?>
-      <script src="<?=BASE_URL?>js/comment.js" defer>
-  <?php } ?>
-  <?php // Displaying login failed alert if loginFailed 
-  if (isset($_SESSION['loginFailed']) && $_SESSION['loginFailed']) { ?>
-      <script>
-          alert('Username or Pasword is wrong');
-      </script>
-  <?php }
-        unset($_SESSION['loginFailed']);
-  ?>
-  <script type="text/javascript">
-    $(function () {
-      $('[data-toggle="tooltip"]').tooltip()
-    });
+    <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
 
-    $('#picupload').on('change',function(){
-        //get the file name
-        var fileName = $(this).val().replace(/^.*\\/, "");
-        console.log(fileName);
-        //replace the "Choose a file" label
-        $(this).next('.custom-file-label').html(fileName);
-    });
-  </script>
+    <script type="text/javascript">
+      // Activate bootstrap Add in
+      $(function () {
+        $('[data-toggle="tooltip"]').tooltip()
+      });
 
-  <?php
-    if( isset($_SESSION['logged_in']) && ($_SESSION['logged_in']) ) {
-        include('modalUpload.php');
-        include('modalCommentImage.php');
-    }
-  ?>
-  <script type="text/javascript">
+      // picupload fucktional
+      $('#picupload').on('change',function(){
+          //get the file name
+          var fileName = $(this).val().replace(/^.*\\/, "");
+          console.log(fileName);
+          //replace the "Choose a file" label
+          $(this).next('.custom-file-label').html(fileName);
+      });
+    </script>
 
-  $('#myModal').on('shown.bs.modal', function () {
-    $('#myInput').trigger('focus')
-  });
+    <?php
+      // Loading script for commentsection in feed
+      if(substr($_SERVER['REQUEST_URI'], -4) === 'feed') {
+        echo "<script src='<?=BASE_URL?>js/comment.js' defer></script>";
+      }
+      // Displaying login failed alert if loginFailed
+      if (isset($_SESSION['loginFailed']) && $_SESSION['loginFailed']) {
+        echo "<script>alert('Username or Pasword is wrong'); </script>";
+      }
+      unset($_SESSION['loginFailed']);
 
-  </script>
-</body>
+      // Loading modal if user is logged_in
+      if( isset($_SESSION['logged_in']) && ($_SESSION['logged_in']) ) {
+          include('modalUpload.php');
+          include('modalCommentImage.php');
+      }
+    ?>
+  </body>
 </html>
