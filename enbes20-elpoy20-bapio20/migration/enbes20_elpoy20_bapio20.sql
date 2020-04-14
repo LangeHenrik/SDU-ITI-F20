ccdc09c1774892d723419a49b9a458ba17547d6b
enzobes <enzobes@me.com>
2020-04-14 23:43:03 +0200

    Update migration for HeidiSQL

--- enbes20-elpoy20-bapio20/migration/enbes20_elpoy20_bapio20.sql
+++ enbes20-elpoy20-bapio20/migration/enbes20_elpoy20_bapio20.sql
@@ -1,32 +1,6 @@
--- phpMyAdmin SQL Dump
--- version 5.0.1
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost
--- Généré le : mar. 14 avr. 2020 à 21:36
--- Version du serveur :  8.0.19
--- Version de PHP : 7.4.2
-
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET AUTOCOMMIT = 0;
-START TRANSACTION;
-SET time_zone = "+00:00";
-
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `enbes20_elpoy20_bapio20`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `images`
---
+DROP DATABASE IF EXISTS enbes20_elpoy20_bapio20;
+CREATE DATABASE enbes20_elpoy20_bapio20;
+USE enbes20_elpoy20_bapio20;
 
 CREATE TABLE `images` (
   `id` int NOT NULL,
@@ -35,72 +9,31 @@ CREATE TABLE `images` (
   `description` text NOT NULL,
   `created` datetime NOT NULL,
   `user_id` int NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
--- --------------------------------------------------------
-
---
--- Structure de la table `user`
---
+);
 
 CREATE TABLE `user` (
   `id_user` int NOT NULL,
   `username` varchar(255) NOT NULL,
   `password` varchar(255) NOT NULL,
   `email` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
---
--- Déchargement des données de la table `user`
---
+);
 
 INSERT INTO `user` (`id_user`, `username`, `password`, `email`) VALUES
 (1, 'UserTest', '$2y$10$5HwiwOFZ2Wr5spaY5GMrdunJ2KiQjmYQqQA9I3Te9WCQT/IsgTPwK', 'usertest@test.com');
 
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `images`
---
 ALTER TABLE `images`
   ADD PRIMARY KEY (`id`),
   ADD KEY `fk_user_id` (`user_id`);
 
---
--- Index pour la table `user`
---
 ALTER TABLE `user`
   ADD PRIMARY KEY (`id_user`);
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `images`
---
 ALTER TABLE `images`
   MODIFY `id` int NOT NULL AUTO_INCREMENT;
 
---
--- AUTO_INCREMENT pour la table `user`
---
 ALTER TABLE `user`
   MODIFY `id_user` int NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `images`
---
 ALTER TABLE `images`
   ADD CONSTRAINT `fk_user_id` FOREIGN KEY (`user_id`) REFERENCES `user` (`id_user`) ON DELETE RESTRICT ON UPDATE RESTRICT;
 COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
