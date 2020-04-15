a0f037f9d547358a4a9cacfb6afadb430e30a5a1
Rasmus Thomsen <rasmt18@student.sdu.dk>
2020-04-15 10:12:16 +0200

    Initial bootstrap styling finished

--- rasmt18_soepe16_matry18/mvc/app/views/image/upload.php
+++ rasmt18_soepe16_matry18/mvc/app/views/image/upload.php
@@ -1,29 +1,36 @@
 <?php include '../app/views/partials/menu.php'; ?>
 
 <div class="content" id="upload">
+    <div class="jumbotron bg-white">
         <h1>Upload page</h1>
-            <h2>Here you can upload your images</h2>
-               <form action="/rasmt18_soepe16_matry18/mvc/public/Image/uploadImage" method ="POST" enctype = "multipart/form-data">
-               <fieldset>
+        <h2>Here you can upload your images</h2>
+        <hr>
+        <form action="/rasmt18_soepe16_matry18/mvc/public/Image/uploadImage" method ="POST" enctype = "multipart/form-data">
+            <div class="form-group">
+                <fieldset>
                     <legend>Upload image:</legend>   
                     <label for="header">Image title:</label>
                     <br>
-                    <input type="text" name="header" placeholder="Select title" autofocus required>
+                    <input class="form-control" type="text" name="header" placeholder="Select title" autofocus required>
                     <br>
                     <label for="description">Description:</label>
                     <br>
-                    <textarea id="description" name="description" placeholder="Here you can write the description for the image." rows="4" cols="50" required></textarea>
+                    <textarea class="form-control" id="description" name="description" placeholder="Here you can write the description for the image." rows="4" cols="50" required></textarea>
                     <br>
                     <label for="image">Select image:</label>
                     <br>
-                    <input type ="file" name ="image" accept=".png,.jpeg" required/>
-                    <input type ="submit" value="Submit" name="submit"/>
+                    <input class="form-control" type ="file" name ="image" accept=".png,.jpeg" required/>
+                    <br>
+                    <input class="btn btn-primary" type ="submit" value="Submit" name="submit"/>
+                    <hr>
                 </fieldset>
-            </form>
-            <div class="response" id="response">
-                <h3><?= $viewbag['response'] ?></h3>    
             </div>
-        <p>If you are having trouble uploading, please contact support.</p>
+        </form>
+        <div class="response" id="response">
+            <h3><?= $viewbag['response'] ?></h3>    
+        </div>
+        <p class="text-info">If you are having trouble uploading, please contact support.</p>
         <br>
-      </form>
-      <?php include '../app/views/partials/foot.php'; ?>
+    </div>
+</div>
+<?php include '../app/views/partials/foot.php'; ?>
