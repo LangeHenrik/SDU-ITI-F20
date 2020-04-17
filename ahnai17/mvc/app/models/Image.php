74f59cb6ea82bb80803de9e556e6677c8e42ae06
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2020-04-17 12:32:52 +0200

    added id sql

--- ahnai17/mvc/app/models/Image.php
+++ ahnai17/mvc/app/models/Image.php
@@ -4,6 +4,7 @@ class Image extends Database {
     
     public function getAllImagesFromUser() {
         $images='SELECT image_id, Title, Description, Image_base64 FROM images where user_id=:id';
+        $id='SELECT id from users where username=:username';
         
         
     }
