ecb29938e342821a9023b8a21b37a1df5386fed7
FrederikH <f.haagensen@hotmail.com>
2020-04-17 12:55:39 +0200

    API work maybe? :)

--- kaseb18frhaa18/mvc/app/models/Picture.php
+++ kaseb18frhaa18/mvc/app/models/Picture.php
@@ -16,6 +16,7 @@ class Picture extends Database {
     }
     
     public function addPicture($user_id){
+        $user_id = filter_var($user_id, FILTER_SANITIZE_STRING);
         $username = filter_var(trim($_POST['json']["username"]), FILTER_SANITIZE_STRING);
         $password = filter_var(trim($_POST['json']["password"]), FILTER_SANITIZE_STRING);
 
@@ -28,24 +29,28 @@ class Picture extends Database {
         $result = $stmt->fetch();
 
         if($username===$result['username'] || password_verify($password, $result['password'])){
-            $title = trim($title);
-            $description = trim($description);
-          
-          
+            $title = $_POST['json']["title"];
+            $description = $_POST['json']["description"];
+            $image = $_POST['json']["image"];
             if (empty($title) || empty($description) || empty($image)) {
-            } elseif ((strlen($title) > 25) or (strlen($description)>250) or ((filesize($image*1.35) > 4294967295))){
+            } elseif ((strlen($title) > 25) or (strlen($description)>250) or ((strlen($image) < 65535*8))){
             } else {
                 $title = filter_var($title, FILTER_SANITIZE_STRING);
                 $description = filter_var($description, FILTER_SANITIZE_STRING);
                 $image = filter_var($image, FILTER_SANITIZE_STRING);
-                $statement = 'insert into feed (head, description, photo, person_id) values(:head, :description, :photo, :person_id)';
-                $stmt->bindParam(':head', $title);
+                $statement = 'insert into feed (title, description, image, user_id) values(:title, :description, :image, :user_id)';
+                $stmt->bindParam(':title', $title);
                 $stmt->bindParam(':description', $description);
-                $stmt->bindParam(':photo', $image);
-                $stmt->bindParam(':person_id', $user_id);
+                $stmt->bindParam(':image', $image);
+                $stmt->bindParam(':user_id', $user_id);
                 $stmt->execute();
 
                 //return image id
+                if ($conn->query($sql) === TRUE) {
+                    $last_id = $conn->insert_id;
+                    $image_id = array("image_id"=>$last_id);
+                    return $image_id;
+                }
             }      
         
         }
