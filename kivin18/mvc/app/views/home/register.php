61e22e73a3aebd06a8d8094db032ecb37639989d
kvinkel <43136038+kvinkel@users.noreply.github.com>
2020-04-18 14:26:48 +0200

    Add js regex

--- kivin18/mvc/app/views/home/register.php
+++ kivin18/mvc/app/views/home/register.php
@@ -2,21 +2,26 @@
 <div class="pb-2 mt-4 mb-2 border-bottom">
     <h1>Register</h1>
 </div>
-<form method="post" action="/kivin18/mvc/public/home/register">
+<form method="post" action="/kivin18/mvc/public/home/register" onsubmit="return checkRegisterFields()">
     <div class="form-group">
         <label for="username">Username</label>
         <input type="text" class="form-control" id="username" name="username" placeholder="Enter username" onkeyup="checkUsername()">
     </div>
     <div class="form-group">
         <label for="password">Password</label>
-        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password">
+        <input type="password" class="form-control" id="password" name="password" placeholder="Enter password" onkeyup="checkPass()">
+    </div>
+    <div class="form-group">
+        <label for="passwordRepeat">Repeat password</label>
+        <input type="password" class="form-control" id="passwordRepeat" name="passwordRepeat" placeholder="Repeat password" onkeyup="confirmPass()">
     </div>
     <button type="submit" class="btn btn-primary">Register</button>
     <a class="btn btn-primary" href="/kivin18/mvc/public/home/">Cancel</a>
-    <small id="usernameInfo" class="form-text text-muted"></small>
 </form>
 <?php if (isset($viewbag['user_info'])) {
     echo $viewbag['user_info'];
 } ?>
+<small id="usernameInfo" class="form-text text-muted"></small>
+<small id="passwordInfo" class="form-text text-muted"></small>
 
 <?php include '../app/views/partials/foot.php'; ?>
--- kivin18/mvc/public/js/js.js
+++ kivin18/mvc/public/js/js.js
@@ -4,7 +4,7 @@ function q(search) {
     return document.querySelector(search);
 }
 
-if(q('.fileInput')) {
+if (q('.fileInput')) {
     q('.fileInput').addEventListener('change', function () {
         let fileName = q('.fileInput').value.split('\\').pop();
         q('#uploadInfo').innerHTML = fileName;
@@ -13,14 +13,63 @@ if(q('.fileInput')) {
 
 // Check username availability
 function checkUsername() {
-    let username = q('input#username').value;
-    fetch('/kivin18/mvc/public/api/checkusername/' + username, {
-        method: 'GET',
-    }).then(function (response) {
-        return response.text();
-    }).then(function (text) {
-        q('small#usernameInfo').innerHTML = text;
-    }).catch(function (error) {
-        q('small#usernameInfo').innerHTML = 'Request failed: ' + error;
-    });
+    if (nameRegex()) {
+        let username = q('input#username').value;
+        fetch('/kivin18/mvc/public/api/checkusername/' + username, {
+            method: 'GET',
+        }).then(function (response) {
+            return response.text();
+        }).then(function (text) {
+            q('small#usernameInfo').innerHTML = text;
+        }).catch(function (error) {
+            console.log('Request failed: ' + error);
+        });
+        return true;
+    } else {
+        return false;
+    }
+}
+
+// Regex
+function nameRegex() {
+    let name = q('input#username').value;
+    let label = q('small#usernameInfo');
+    let regex = new RegExp(/^\S\w{5,50}$/);
+    if (regex.test(name)) {
+        label.innerHTML = '';
+        return true;
+    } else {
+        label.innerHTML = 'Username must be between 5-50 characters. No spaces allowed!';
+        return false;
+    }
+}
+
+function checkPass() {
+    let pass = q('input#password').value;
+    let label = q('small#passwordInfo');
+    let regex = new RegExp(/(?=.*[a-z])(?=.*[A-Z])(?=.*[0-9])(?=.*[!@#\$%\^&\*])(?=.{8,})/);
+    if (regex.test(pass)) {
+        label.innerHTML = '';
+        return true;
+    } else {
+        label.innerHTML = 'Password must contain at least 8 characters, 1 lower case, 1 upper case, 1 numeric and 1 of these special characters: !@#$%^&*';
+        return false;
+    }
+}
+
+function confirmPass() {
+    let pass = q('input#password').value;
+    let passRepeat = q('input#passwordRepeat').value;
+    let label = q('small#passwordInfo');
+    if (passRepeat === pass) {
+        label.innerHTML = "";
+        return true;
+    } else {
+        label.innerHTML = "Passwords don't match!";
+        return false;
+    }
+}
+
+function checkRegisterFields() {
+    return checkUsername() && checkPass() && confirmPass();
 }
