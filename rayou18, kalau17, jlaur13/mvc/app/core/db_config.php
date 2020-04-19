4834ff04db8ab52eae49bc4c6fcd73c2b440b053
Symaro <johnlaursen90@gmail.com>
2020-04-19 20:28:24 +0200

    redirect after login still faulty

--- rayou18, kalau17, jlaur13/mvc/app/core/db_config.php
+++ rayou18, kalau17, jlaur13/mvc/app/core/db_config.php
@@ -3,8 +3,8 @@ class DB_Config {
 	
 	protected $servername = 'localhost';
 
-	protected $username = 'php';
-	protected $password = '1234';
+	protected $username = 'root';
+	protected $password = '';
 	protected $dbname = 'rayou18_kalau17_jlaur13_assignment2';
 
 }
