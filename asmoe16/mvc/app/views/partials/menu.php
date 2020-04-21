bb3a681078a6618224c75eeccc7eae50b615d7b6
Aske Bækdal Møller <askebm@live.dk>
2020-04-21 10:29:14 +0200

    bootstrap

--- asmoe16/mvc/app/views/partials/menu.php
+++ asmoe16/mvc/app/views/partials/menu.php
@@ -1,14 +1,17 @@
 <html>
-    <head>
-    <script src="../js/js.js"></script>
-    </head>
-    <body>
+  <head>
+	<!-- Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css"
+		integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
+  <script src="../js/js.js"></script>
+  </head>
+  <body>
 
 <div style="background-color: lightblue;">Menu partial view</div>
 
 <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']) : ?>
 
-<a href="/Henrik/mvc/public/user/logout">log out</a>
+<a href="/asmoe16/mvc/public/user/logout">log out</a>
 
 <?php endif; ?>
 
