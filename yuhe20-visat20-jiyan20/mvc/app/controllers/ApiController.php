79b3213d0d72aaa0d93aa4821d4cd82bccc2712e
YuhengHe <50586123+YuhengHe@users.noreply.github.com>
2020-04-21 21:44:46 +0800

    Modified ApiController

--- yuhe20-visat20-jiyan20/mvc/app/controllers/ApiController.php
+++ yuhe20-visat20-jiyan20/mvc/app/controllers/ApiController.php
@@ -18,6 +18,18 @@ class ApiController extends Controller {
 		echo json_encode($users, JSON_PRETTY_PRINT);
 	}
 
+	public function images ($user, $user_id) {
+		if ($this->post()) {
+			$UP_info = json_decode($_POST['json'], true);
+			print_r($UP_info);
+			$UP_info['userid'] = $user_id;
+			if($this->model('user')->verifyUser($UP_info)) {
+				$user_image = $this->model('image')->ApiUploadImage($UP_info);
+			} elseif ($this->get()) {
+				 $result = $this->model('image')->getUserImages($user_id);
+				 echo json_encode($result,JSON_PRETTY_PRINT);
+		}
 
+	}
 
 }
