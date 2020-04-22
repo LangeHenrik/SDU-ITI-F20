d183e6b9508b20fb5bb15dc0c6752c4dbea7b669
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2020-04-22 11:50:55 +0200

    added title

--- ahnai17/mvc/app/views/Feed/feed.php
+++ ahnai17/mvc/app/views/Feed/feed.php
@@ -4,9 +4,10 @@ include_once '../app/views/partials/header.php';?>
 <div id="floated-imgs">
 <?php
     for ($i=0; $i < count($viewbag); $i++) { ?> 
-<img src="<?=$viewbag[$i][3]?>" title="<?=$viewbag[$i][2]?>"
+<img src="<?=$viewbag[$i][3]?>" title="<?=$viewbag[$i][1]?>"
      alt="<?=$viewbag[$i][1]?>" height="100px" width="150px">
-Description: <?=$viewbag[$i][1]?>
+<p>Title: <?=$viewbag[$i][1]?> </p>
+<p>Description: <?=$viewbag[$i][2]?></p><br>
     <?php }
 ?></div> <?php
 include_once '../app/views/partials/foot.php';
