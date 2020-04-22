dc281d4b8ed7f23b94d5d932807e56719d31dffc
Aske Bækdal Møller <askebm@live.dk>
2020-04-22 10:12:26 +0200

    Initial image controller

---
+++ asmoe16/mvc/app/controllers/ImageController.php
@@ -0,0 +1,9 @@
+<?php
+
+class ImageController extends Controller {
+	
+	public function index () {
+		//This is a proof of concept - we do NOT want HTML in the controllers!
+		$this->view('home/index');
+	}
+}
