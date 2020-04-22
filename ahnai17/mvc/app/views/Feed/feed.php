c967b4ddeba5e546fc74df42c5116d49b055087d
Ahmadullah Naibi <ahnai17@student.sdu.dk>
2020-04-22 11:27:07 +0200

    added images below each other

--- ahnai17/mvc/app/views/Feed/feed.php
+++ ahnai17/mvc/app/views/Feed/feed.php
@@ -1,8 +1,12 @@
 <?php
-include_once '../app/views/partials/header.php';
+include_once '../app/views/partials/header.php';?>
 
+<div id="floated-imgs">
+<?php
     for ($i=0; $i < count($viewbag); $i++) { ?> 
-        <img src="<?=$viewbag[$i][3]?>" alt="<?=$viewbag[$i][1]?>" height="200px" width="300px"> </br>
+<img src="<?=$viewbag[$i][3]?>" title="<?=$viewbag[$i][2]?>"
+     alt="<?=$viewbag[$i][1]?>" height="100px" width="150px">
+Description: <?=$viewbag[$i][1]?>
     <?php }
-
+?></div> <?php
 include_once '../app/views/partials/foot.php';
