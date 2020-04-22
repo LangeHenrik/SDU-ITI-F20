dcada5767da31370ec10e1b1b7a7b961ffee2584
Aske Bækdal Møller <askebm@live.dk>
2020-04-22 09:59:51 +0200

    Created dynamic menu based on log in status

--- asmoe16/mvc/app/views/partials/menu.php
+++ asmoe16/mvc/app/views/partials/menu.php
@@ -10,11 +10,28 @@
   </head>
   <body>
 
+<!--
 <div style="background-color: lightblue;">Menu partial view</div>
+-->
 
-<?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']) : ?>
 
-<a href="/asmoe16/mvc/public/user/logout">log out</a>
-
-<?php endif; ?>
-
+<!-- Menu links -->
+<nav class="navbar navbar-expand-sm bg-primary navbar-dark">
+	<ul class="navbar-nav">
+		<?php
+			function fpage($vis_name,$page) {
+				echo "<li class='nav-item'>
+					<a class='nav-link' href=/asmoe16/mvc/public/$page>$vis_name</a>
+					</li>";
+			}
+			fpage("Home","home/index");
+			fpage("Images","image/index");
+			if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']) {
+				fpage("log out","user/logout");
+			} else {
+				fpage("Register","user/registration");
+				fpage("Log in","user/loginpage");
+			}
+		?>
+	</ul>
+</nav>
