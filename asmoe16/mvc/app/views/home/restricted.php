8f94632ebf034361b4e564c91cbfdcd32eac0d89
Aske Bækdal Møller <askebm@live.dk>
2020-04-22 10:11:41 +0200

    Added a page for when accesing restricted pages

---
+++ asmoe16/mvc/app/views/home/restricted.php
@@ -0,0 +1,9 @@
+<?php include '../app/views/partials/menu.php'; ?>
+
+<div class="container">
+	<div class="alert alert-danger">
+		<strong>Acces Denied!</strong> You need to be logged in to acces this page.
+	</div>
+</div>
+
+<?php include '../app/views/partials/foot.php'; ?>
