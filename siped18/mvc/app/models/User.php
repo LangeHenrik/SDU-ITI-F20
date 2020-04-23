e1b87244464745aa55e0e836b72cebe88fa86c24
Signetp <siped18@student.sdu.dk>
2020-04-23 14:04:44 +0200

    login

--- siped18/mvc/app/models/User.php
+++ siped18/mvc/app/models/User.php
@@ -1,6 +1,5 @@
 <?php
-$dbFile = '/Users/signethuesenpedersen/Documents/SDU-ITI-F20/siped18/mvc/app/core/Database.php';
-require_once $dbFile;
+
 class User extends Database
 {
     public function login($username, $password)
@@ -11,22 +10,13 @@ class User extends Database
         $stmt->bindParam(':username', $username);
         $stmt->execute();
 
-        $result = $stmt->fetch(); //fetchAll to get multiple rows
+        $result = $stmt->fetchAll(); //fetchAll to get multiple rows
 
-        if (count($result) == 1) {
-            $username = $row["username"];
-            $uPassword = $row["uPassword"];
-    
-            if (password_verify($password, $uPassword)) {
-                $_SESSION['loggedIn'] = true;
-                $_SESSION['username'] = $username;
-                header("Location: imagefeed.php");
-            } else {
-                $_SESSION['loggedIn'] = false;
-                $errorMessage = "Wrong username or password";
+        foreach ($result as $value) {
+            if ($value['username'] == $username && password_verify($password, $value['password'])) {
+                return true;
             }
         }
-        return false;
     }
 
     public function getAll()
@@ -41,52 +31,20 @@ class User extends Database
         return $result;
     }
 
-    public function checkLogin($username, $password) 
-    {
-    
-        $sql = "SELECT uPassword FROM users WHERE username = :username";
-
-        $stmt = $this->conn->prepare($sql);
-        $stmt->bindParam(':username', $username);
-        $stmt->execute();
-
-        $result = $stmt->fetch();
-
-        if(isset($result))
-        {
-            return password_verify($password, $result);
-
-        } else {
-            return false;
-        }
-    }
-
-    public function register()
+    public function register($firstname, $lastname, $email, $username, $password)
     {  
-        $chk_u = $conn->prepare("SELECT username FROM user WHERE username = :username");
-        $chk_u->bindParam(':username', $username);
+        $hpassword = password_hash($password, PASSWORD_DEFAULT);
         
-        $chk_e = $conn->prepare("SELECT email FROM user WHERE email = :email");
-        $chk_e->bindParam(':email', $email);
+        $sql = "INSERT INTO users (firstname, lastname, username, email, username, password) VALUES (:firstname, :lastname, :email, :username, :password)";
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindParam(':firstname', $firstname);
+        $stmt->bindParam(':lastname', $lastname);
+        $stmt->bindParam(':email', $email);
+        $stmt->bindParam(':username', $username);
+        $stmt->bindParam(':password', $hpassword);
         
-        $chk_u->execute();
-        $chk_e->execute();
-        
-        if ($chk_u->rowCount() > 0) 
-        {
-            print("Sorry ");
-        } elseif ($chk_e->rowCount() > 0) {
-            print("Sorry this email is already asign to a user");
-        } else {
-            $stmt = $conn->prepare("INSERT INTO user (firstname, lastname, username, email, password)
-            VALUES (:firstname, :lastname, :username, :email, :password)");
-
-            $stmt->bindParam(':firstname', $_POST['firstname']);
-            $stmt->bindParam(':lastname', $_POST['lastname']);
-            $stmt->bindParam(':username', $_POST['username']);
-            $stmt->bindParam(':email', $_POST['email']);
-            $stmt->bindParam(':password', $_POST['password']);
-            $result = $stmt->execute();
+        $stmt->execute();
+        return true;
         }  
     }
 }
