1ff3f4f735ebc800d8a80c8471ad08ad619252ed
Symaro <37690011+Symaro@users.noreply.github.com>
2020-04-24 13:23:18 +0200

    Update index.php

--- rayou18_kalau17_jlaur13/mvc/app/views/home/index.php
+++ rayou18_kalau17_jlaur13/mvc/app/views/home/index.php
@@ -2,46 +2,48 @@
 <html lang="en">
 <head>
 
-    <?php include '../app/views/partials/header.php'?>
+    <?php include '../app/views/partials/header.php' ?>
     <link rel="stylesheet" href="/rayou18_kalau17_jlaur13/mvc/public/styles/background.css">
     <link rel="stylesheet" href="/rayou18_kalau17_jlaur13/mvc/public/styles/homepage.css">
     <title>NetJohnPany</title>
 
 </head>
 <body>
-<?php include '../app/views/partials/menu.php'; ?>
-<div class="container" >
-    <div class="jumbotron" >
-        <h1 class="display-4">Welcome to our website</h1>
-        <p class="lead">Hello there<?php if($viewbag['username'] != null)echo ', ' . $viewbag['username']?></p>
-        <hr class="my-4">
-        <p>Go to the image feed</p>
+    <?php include '../app/views/partials/menu.php'; ?>
+    <div class="container">
+        <div class="jumbotron">
+            <h1 class="display-4">Welcome to our website</h1>
+            <p class="lead">Hello there<?php
+                if ($viewbag['username'] != null) {
+                echo ', ' . $viewbag['username'] ?></p>
+            <hr class="my-4">
+            <p>Go to the image feed</p>
+            <a class="btn btn-primary btn-md" href="/rayou18_kalau17_jlaur13/mvc/public/imageFeed" role="button">image feed</a>
+            <?php }
+            else { ?>
+                <hr class="my-4">
+                <p> Please login to gain access</p>
+                <a  class="btn btn-primary btn-md" onclick="callAjax()" href="#" role="button">Login</a>
+            <?php } ?>
 
-            <a class="btn btn-primary btn-md" href="#" role="button">image feed</a>
+
+        </div>
 
     </div>
 
-</div>
+    <?php if ($viewbag['restricted']) {
+        echo "<br>";
+        echo "<img src='/rayou18_kalau17_jlaur13/mvc/public/Resources/classified.png' class=\"img-fluid\" alt='classified' />";
+        echo "<br>";
+        echo "You tried to access a classified page.";
+    }
+    ?>
 
 
+    <div id="ajaxHolder">
+    </div>
 
 
-Hello there, <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true){echo $viewbag['username'];}  ?>
-
-<?php if($viewbag['restricted'])
-{
-    echo "<br>";
-    echo "<img src='/rayou18_kalau17_jlaur13/mvc/public/Resources/classified.png' class=\"img-fluid\" alt='classified' />";
-    echo "<br>";
-    echo "You tried to access a classified page.";
-}
-?>
-
-
-<div id="ajaxHolder">
-</div>
-
-
-<?php include '../app/views/partials/foot.php'; ?>
+    <?php include '../app/views/partials/foot.php'; ?>
 </body>
 </html>
