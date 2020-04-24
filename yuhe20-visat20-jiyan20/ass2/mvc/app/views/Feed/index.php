12a77bc8d427af6a591dbb30136f724c7c5106d2
YuhengHe <50586123+YuhengHe@users.noreply.github.com>
2020-04-24 22:34:52 +0800

    create feed pages, mostly are the original codes from ass1

---
+++ yuhe20-visat20-jiyan20/ass2/mvc/app/views/Feed/index.php
@@ -0,0 +1,9 @@
+<?php include '../app/views/partials/menu.php'; ?>
+<div class="content" id="content">
+    <div>
+        <h1>WELCOME TO THE IMAGE FEED PAGE</h1>
+        <h3>BELOW YOU CAN FIND PICTURES SHARED BY OUR USERS</h3>
+        <button type="button" id="load" name="load" onclick="return getUserImage()">Discover</button>
+        <hr>
+    </div>
+</div>
