2d327730be496a405dea4cd74b2ac4a22dab6147
Kasper <43139475+KasperDelaxson@users.noreply.github.com>
2020-04-24 10:16:14 +0200

    Added js for Find User

--- kaseb18frhaa18/mvc/app/views/partials/header.php
+++ kaseb18frhaa18/mvc/app/views/partials/header.php
@@ -7,6 +7,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta charset="UTF-8" />
     <script src="/kaseb18frhaa18/mvc/public/js/RegexInputChecker.js"></script>
+    <script src="/kaseb18frhaa18/mvc/public/js/FindUsers.js"></script>
     <link rel="stylesheet" type="text/css" href="/kaseb18frhaa18/mvc/public/css/style.css"> 
 </head>
 <?php include('navbar.php'); ?>
