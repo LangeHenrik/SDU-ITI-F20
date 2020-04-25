3987bb64e132b8f440506d8d3854fc1b5a90384b
JakobKaae <jakaa18@student.sdu.dk>
2020-04-25 19:22:04 +0200

    v.1 login view

--- jakaa18/Assignment1/mvc/app/views/home/login.php
+++ jakaa18/Assignment1/mvc/app/views/home/login.php
@@ -1,11 +1,38 @@
-<?php include '../app/views/partials/menu.php'; ?>
+<?php
+$title = "Login page";
+include '../app/views/partials/menu.php';
+
+
+?>
+
+    <div class="grid-container">
+        <div class="login1">
+            <h1><?php echo 'Log in or sign up via the Registration button' ?></h1>
+        </div>
+        <div class="login2" id="loginView">
+            <!-- Login -->
+            <form method="post">
+                <p> Username: <input type="text" name="username" id="usernameId" placeholder="Username"></p><br>
+                <p> Password: <input type="password" name="password" id="passwordId" placeholder="Password"></p><br>
+
+        </div>
+        <div class="login3">
+            <input type="submit" class="button" name="send" value="Login" id="loginBtn">
+        </div>
+        </form>
+
+        <div class="login4">
+            <button onclick="window.location.href = '/jakaa18/Assignment1/mvc/public/home//Register';">Register here</button>
+        </div>
+
+    </div>
 
 <br/>
-You are now logged in!
+<!--You are now logged in!
 <br><br>
 <form method="POST" action="/Exercises/mvc/public/home/logout">
 	<input type="submit" value="log out"/>
-</form>
+</form>-->
 
 
 <?php include '../app/views/partials/foot.php'; ?>
