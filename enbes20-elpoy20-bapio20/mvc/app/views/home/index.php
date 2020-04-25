ae7359d3be2c837fa7a32313d1b719ca8e4ac35a
elpoy20 <elpoy20@student.sdu.dk>
2020-04-25 16:47:09 +0200

    Home container

--- enbes20-elpoy20-bapio20/mvc/app/views/home/index.php
+++ enbes20-elpoy20-bapio20/mvc/app/views/home/index.php
@@ -1,7 +1,9 @@
 <!-- Login form -->
 <div id="main-content">
   <div class="container">
+
     <div class="title"><?= $viewbag['title']?></br></div>
+    <div class="form">
     <?php
           if(isset($_SESSION['id'])){
               echo '<h2>Welcome '. $_SESSION['username'] .'</h2>
@@ -9,8 +11,9 @@
           }
           else{
               echo '<h3>Please Login to continue!</h3>
-                    <a href="'.URL.'home/login">login</a>';
+                    <a href="'.URL.'home/login">Login</a>';
           }
         ?>
+      </div>
   </div>
 </div>
