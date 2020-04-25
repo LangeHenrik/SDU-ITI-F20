f56ac482c4d7d4c044b4a747b80d73353de6619c
Kasper <43139475+KasperDelaxson@users.noreply.github.com>
2020-04-25 15:58:00 +0200

    fix userlist

--- kaseb18frhaa18/mvc/app/views/home/userList.php
+++ kaseb18frhaa18/mvc/app/views/home/userList.php
@@ -2,22 +2,21 @@
 
 
 <div class="wrapper">
-    <div class="contentlist">
+    <div class="contentlist" id="nig">
         <form>
             Find user: <input type="text" onkeyup="findUsers(this.value)">
         </form>
-        <div>
-            <table class="userlist" id="users">
-                <?php
-                $hint = "<tr><th>Name</th>
+        <table class="userlist" id="users">
+            <?php
+            $hint = "<tr><th>Name</th>
             <th>Username</th>
             </tr>";
-                foreach ($viewbag as $name) {
-                    $hint .= "<tr> <td> $name[name] </td> <td> $name[username] </td> <tr>";
-                }
-                echo $hint;
-                ?>
-            </table>
-        </div>
+            foreach ($viewbag as $name) {
+                $hint .= "<tr> <td> $name[name] </td> <td> $name[username] </td> <tr>";
+            }
+            echo $hint;
+            ?>
+        </table>
+
     </div>
 </div>
