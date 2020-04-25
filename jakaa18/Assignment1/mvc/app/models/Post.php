1576f6b335cd33d4fbb90cde6c57eba16a4ee3b6
JakobKaae <jakaa18@student.sdu.dk>
2020-04-25 16:34:55 +0200

    Removed 1 parameter. DB increments by itself

--- jakaa18/Assignment1/mvc/app/models/Post.php
+++ jakaa18/Assignment1/mvc/app/models/Post.php
@@ -1,11 +1,11 @@
 <?php
 class Post extends Database
 {
-    public function newPicPost($pic_id, $header, $description, $username, $picture, $encode_image = true){
+    public function newPicPost($header, $description, $username, $picture, $encode_image = true){
         if ($encode_image) {
             $picture = base64_encode($picture);
         }
-        $this->query('INSERT INTO pictures (pic_id, header, description, user, picture) VALUES (?, ?, ?, ?, ?);', [$pic_id, $header, $description, $username, $picture]);
+        $this->query('INSERT INTO pictures (header, description, user, picture) VALUES (?, ?, ?, ?, ?);', [$header, $description, $username, $picture]);
 
         return $this->conn->lastInsertId();
     }
