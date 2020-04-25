29a8fed6004c2cde4b80a437ca034100069ad7e7
JakobKaae <jakaa18@student.sdu.dk>
2020-04-25 20:05:41 +0200

    JS to show if username is available

---
+++ jakaa18/Assignment1/mvc/public/js/NameAvailability.js
@@ -0,0 +1,22 @@
+function UsernameAvailable($username) {
+    let xmlhttp = new XMLHttpRequest();
+    let info = document.getElementById("username-availability");
+    let input = document.getElementById("username");
+    xmlhttp.onreadystatechange = function() {
+        if (this.readyState === 4 && this.status === 200) {
+            info.innerHTML = this.responseText;
+            if (this.responseText.includes('available')){
+                info.style.color = "green";
+                input.style.borderColor = "green";
+                return true;
+            }
+            info.style.color = "red";
+            input.style.borderColor = "red";
+            return false;
+        }
+    };
+    if ($username.length > 2){
+        xmlhttp.open("GET", "/mvc/public/api/available/username/" + $username, true);
+        xmlhttp.send();
+    }
+}
