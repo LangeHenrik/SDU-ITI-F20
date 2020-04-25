ce56d2e3cbd8fc8401beddedd680f9ff0ac96e98
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2020-04-25 15:14:13 +0200

    Page fixes

--- jakaa18/Assignment1/mvc/app/views/partials/sidebar.php
+++ jakaa18/Assignment1/mvc/app/views/partials/sidebar.php
@@ -1,9 +1,9 @@
 
 <div class="homepage1">
     <ul>
-        <li><a onclick="showHomeLink()" href="#" id="homelink">Home</a></li>
+        <li><a href="/Assignment1/mvc/public/user/home" id="homelink">Home</a></li>
         <li><a href="/Assignment1/mvc/public/user/upload" id="photolink">Upload Photos</a></li>
-        <li><a onclick="showUsersLink()" href="#" id="userslink">Users</a></li>
+        <li><a href="/Assignment1/mvc/public/user/userlist" id="userslink">Users</a></li>
         <li><a href="/Assignment1/mvc/public/user/logout" id="logout">Logout</a></li>
     </ul>
 </div>
