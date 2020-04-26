6eb5938a7447775a9307536eff218e2c95dc349a
Nicholai Bjerke Jensen <nbj@rhost.dk>
2020-04-26 19:23:59 +0200

    fixed migration.sql

--- njens16/mvc/migration.sql
+++ njens16/mvc/migration.sql
@@ -16,12 +16,3 @@ CREATE TABLE image(
     upload_time DATETIME DEFAULT CURRENT_TIMESTAMP 
 );
 
-CREATE USER "njens16"@"localhost" IDENTIFIED BY "mypassword";
-GRANT ALL PRIVILEGES ON njens16 . * TO "njens16"@"localhost";
-FLUSH PRIVILEGES;
-
-use njens16;
-INSERT INTO user(username, password) VALUES ("test", ENCRYPT("test"));
-INSERT INTO user(username, password) VALUES ("test1", ENCRYPT("test"));
-INSERT INTO user(username, password) VALUES ("test2", ENCRYPT("test"));
-INSERT INTO user(username, password) VALUES ("test3", ENCRYPT("test"));
