37a9e3a0dc8167bca91e70a0ff1bd4ba5e8eaa9b
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-04-26 10:56:00 +0200

    method fix

--- rasmt18_soepe16_matry18/mvc/app/core/Restricted.php
+++ rasmt18_soepe16_matry18/mvc/app/core/Restricted.php
@@ -10,7 +10,7 @@ function restricted ($controller, $method) {
 
 	if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
 		return false;
-	} else if( isset($controller) && in_array($method, $restricted_urls[$controller])) {
+	} else if( isset($controller) && in_array(strtolower($method), $restricted_urls[$controller])) {
 		return true;
 	} else {
 		return false;
