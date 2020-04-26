b601b76abe4da96431fdfee9439291fcd5123718
frederiktt <32263257+frederiktt@users.noreply.github.com>
2020-04-26 20:04:48 +0200

    Polish :)

--- frtol07/mvc/app/models/ImageFeed.php
+++ frtol07/mvc/app/models/ImageFeed.php
@@ -1,7 +1,4 @@
 <?php
-//if (session_status() == PHP_SESSION_NONE) {
-//    session_start();
-//}
 
 class ImageFeed extends Database
 {
--- frtol07/mvc/app/models/ImageFeedblobs.php
+++ frtol07/mvc/app/models/ImageFeedblobs.php
@@ -4,13 +4,6 @@ class ImageFeedblobs extends Database
 {
 
     public function showImages() {
-//        $getPictureStmt = $this->conn->prepare("SELECT name,user, description, header, image  FROM imagesblob");
-//
-//        $queryExecuted = $getPictureStmt->execute();
-//        $getPictureStmt->setFetchMode(PDO::FETCH_ASSOC);
-//        $result = $getPictureStmt->fetchAll();
-//
-//        return $result;
         $stmt = $this->conn->query("SELECT name,user, description, title, image  FROM imagesblob");
         $result = $stmt->fetchAll();
         return $result;
