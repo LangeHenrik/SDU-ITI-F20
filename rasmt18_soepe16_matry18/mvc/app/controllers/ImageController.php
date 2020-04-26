42418dc34050625d7e37118901e25905e19ea427
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-04-26 11:52:20 +0200

    restricted fix

--- rasmt18_soepe16_matry18/mvc/app/controllers/ImageController.php
+++ rasmt18_soepe16_matry18/mvc/app/controllers/ImageController.php
@@ -17,6 +17,11 @@ class ImageController extends Controller {
     }
 
     public function loadImages() {
-        $this->model('Image')->loadImagesFromModel();
+        if(isset($_SESSION['logged_in']) && $_SESSION['logged_in'] == true) {
+            $this->model('Image')->loadImagesFromModel();
+        } else{
+            echo "You are not logged in so you can not load images. \nPlease log in";
+        }
+        
     }
 }
