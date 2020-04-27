ca404250ce8005ecf99f02fe3d34906f0b4aa8e8
JakobKaae <jakaa18@student.sdu.dk>
2020-04-27 21:07:20 +0200

    Text

--- jakaa18/Assignment1/mvc/app/views/home/Feed.php
+++ jakaa18/Assignment1/mvc/app/views/home/Feed.php
@@ -20,7 +20,7 @@ include '../app/views/partials/menu.php';
         <?php
         foreach ($viewbag['posts'] as $post){
             print '<div class="post container">';
-            print $post['user'].'</br><h2>'. $post['header'] .'</h2></br>';
+            print "Uploaded by: " . $post['user'].'</br><h2>'. $post['header'] .'</h2></br>';
             print '<img class="main-image" src="data:image/jpeg;base64,' .  $post['picture'] . '" />';
             print '</br>'.$post['description'].'</br>';
 
