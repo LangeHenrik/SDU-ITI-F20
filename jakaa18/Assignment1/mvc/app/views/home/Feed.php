51a30dc0e3d4439af9cc92dcc510b8adc29e37ca
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2020-04-27 21:10:20 +0200

    Prettify feed

--- jakaa18/Assignment1/mvc/app/views/home/Feed.php
+++ jakaa18/Assignment1/mvc/app/views/home/Feed.php
@@ -1,33 +1,31 @@
 <?php
-$title = "Login page";
-include '../app/views/partials/menu.php';
-
+    include '../app/views/partials/menu.php';
 ?>
-    <style>
-        <?php include '../../css/style.css'; ?>
-    </style>
+<style>
+    <?php include '../../css/style.css'; ?>
+</style>
 
-
-    <div class="grid-container2">
-<div class="header">
-    <h1><?php echo 'Welcome to your homepage!' ?></h1>
-</div>
+<div class="grid-container2">
+    <div class="header">
+        <h1><?php echo 'Welcome to your homepage!' ?></h1>
+    </div>
 
     <?php include '../app/views/partials/sidebar.php'?>
 
-<div class="homepage2" name="Image_page" id="imagePage">
-    <div class="image-container">
-        <?php
-        foreach ($viewbag['posts'] as $post){
-            print '<div class="post container">';
-            print "Uploaded by: " . $post['user'].'</br><h2>'. $post['header'] .'</h2></br>';
-            print '<img class="main-image" src="data:image/jpeg;base64,' .  $post['picture'] . '" />';
-            print '</br>'.$post['description'].'</br>';
+    <div class="homepage2" name="Image_page" id="imagePage">
+        <div class="image-container">
+            <?php
+            foreach ($viewbag['posts'] as $post){
+                print '<div class="post container">';
+                print "Uploaded by: " . $post['user'].'</br><h2>'. $post['header'] .'</h2></br>';
+                print '<img class="main-image" src="data:image/jpeg;base64,' .  $post['picture'] . '" />';
+                print '</br>'.$post['description'].'</br>';
 
-            print '</div>';
+                print '</div>';
 
-        }
-        ?>
+            }
+            ?>
+        </div>
     </div>
 </div>
 
