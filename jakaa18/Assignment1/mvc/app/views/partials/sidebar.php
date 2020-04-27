eaaacf4940c487673ef3ea4144ff7d893d44153b
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2020-04-27 21:17:28 +0200

    remove from external folder

--- jakaa18/Assignment1/mvc/app/views/partials/sidebar.php
+++
@@ -1,9 +0,0 @@
-
-<div class="homepage1">
-    <ul>
-        <li><a href="/Assignment1/mvc/public/home" id="homelink">Home</a></li>
-        <li><a href="/Assignment1/mvc/public/upload" id="photolink">Upload Photos</a></li>
-        <li><a href="/Assignment1/mvc/public/userlist" id="userslink">Users</a></li>
-        <li><a href="/Assignment1/mvc/public/user/logout" id="logout">Logout</a></li>
-    </ul>
-</div>
