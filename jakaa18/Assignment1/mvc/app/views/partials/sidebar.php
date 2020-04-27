8cbc70a62cd5880c6fe76310a637b925571429dc
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2020-04-27 13:34:53 +0200

    Better redirect

--- jakaa18/Assignment1/mvc/app/views/partials/sidebar.php
+++ jakaa18/Assignment1/mvc/app/views/partials/sidebar.php
@@ -1,7 +1,7 @@
 
 <div class="homepage1">
     <ul>
-        <li><a href="/Assignment1/mvc/public/home/Feed" id="homelink">Home</a></li>
+        <li><a href="/Assignment1/mvc/public/home" id="homelink">Home</a></li>
         <li><a href="/Assignment1/mvc/public/user/upload" id="photolink">Upload Photos</a></li>
         <li><a href="/Assignment1/mvc/public/user/userlist" id="userslink">Users</a></li>
         <li><a href="/Assignment1/mvc/public/user/logout" id="logout">Logout</a></li>
