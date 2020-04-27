500f5986f12dc09f1486e3a382f92f84d519ccb4
Nicholai Bjerke Jensen <nbj@rhost.dk>
2020-04-27 09:17:48 +0200

    added curl api scripts

---
+++ njens16/api-demo/getImages.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+curl -G http://localhost:8080/njens16/mvc/public/api/pictures/user/5 | jq
---
+++ njens16/api-demo/imageUpload.sh
@@ -0,0 +1,2 @@
+#!/usr/bin/bash
+curl --data-urlencode json@image.json http://localhost:8080/njens16/mvc/public/api/pictures/user/3
