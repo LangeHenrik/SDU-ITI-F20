d0f2cc2a142fa2a06eb3250278e7603f1438cbbe
JakobKaae <jakaa18@student.sdu.dk>
2020-04-27 13:23:25 +0200

    Hid the href logout

--- jakaa18/Assignment1/mvc/app/views/partials/menu.php
+++ jakaa18/Assignment1/mvc/app/views/partials/menu.php
@@ -6,7 +6,7 @@
 
 <?php if(isset($_SESSION['logged_in']) && $_SESSION['logged_in']) : ?>
 
-<a href="/Assignment1/mvc/public/user/logout">log out</a>
+<!--<a href="/Assignment1/mvc/public/user/logout">log out</a>-->
 
 <?php endif; ?>
 
