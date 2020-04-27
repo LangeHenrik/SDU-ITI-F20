9ca2e0133a2a3664e016061adac5d81a6796f232
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2020-04-27 12:52:26 +0200

    Replace sidebar and error message

--- jakaa18/Assignment1/mvc/app/views/home/Feed.php
+++ jakaa18/Assignment1/mvc/app/views/home/Feed.php
@@ -9,18 +9,12 @@ include '../app/views/partials/menu.php';
 
 
     <div class="grid-container2">
-    <body onload="showHomeLink()">
 <div class="header">
     <h1><?php echo 'Welcome to your homepage!' ?></h1>
 </div>
-<div class="homepage1">
-    <ul>
-        <li><a onclick="showHomeLink()" href="#" id="homelink">Home</a></li>
-        <li><a onclick="showPhotoLink()" href="#" id="photolink">Upload Photos</a></li>
-        <li><a onclick="showUsersLink()" href="#" id="userslink">Users</a></li>
-        <li><a href="logout.php" id="logout">Logout</a></li>
-    </ul>
-</div>
+
+<?php include '../app/views/partials/sidebar.php'?>
+
 <div class="homepage2" id="uploadForm">
     <form action="upload.php" method="post" enctype="multipart/form-data">
         Select image to upload
