cb4f8f164e1077c8c6954497933d216ebff9d42d
Jesper Isgaard Hansen <43139456+IbberSnitte@users.noreply.github.com>
2020-04-27 18:24:42 +0200

    Userlist setup

---
+++ jakaa18/Assignment1/mvc/app/views/Userlist/index.php
@@ -0,0 +1,17 @@
+<?php
+include '../app/views/partials/menu.php';
+?>
+    <style>
+        <?php include '../../css/style.css'; ?>
+    </style>
+<?php
+    include '../app/views/partials/pagegrid.php';
+    include '../app/views/partials/sidebar.php';
+
+?>
+
+<div class="header">
+    <h1><?php echo "Here's a list of all the users on here" ?></h1>
+</div>
+<?php include '../app/views/partials/foot.php'; ?>
+
