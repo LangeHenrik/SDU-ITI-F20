c1942191ab4894f153035a8431ce2d2417bd7e78
channddra <43722552+channddra@users.noreply.github.com>
2020-04-28 14:15:25 +0200

    Revert "correct API function call typo"
    This reverts commit 43f2bff628d211834c70e5429eff166a6fc73587.

--- alhen20_chset20/mvc/app/controllers/APIController.php
+++ alhen20_chset20/mvc/app/controllers/APIController.php
@@ -31,22 +31,17 @@ Returns a list of pictures for a specific user";
 				try{
 					$json = json_decode($_POST['json']);
 					$image = $json->image;
-					$title = $json->title;
-					$description = $json->description;
-					$username = $json->username;
-					$password = $json->password;
+					$title = filter_var($json->title, FILTER_SANITIZE_STRING);
+					$description =filter_var($json->description, FILTER_SANITIZE_STRING);
+					$username = filter_var($json->username, FILTER_SANITIZE_STRING);
+					$password =filter_var($json->password, FILTER_SANITIZE_STRING);
 				} catch (Exception $e){
 					die("Error");
 				}
 				$testuser = $this-> model('User');
-
-				if ($testuser->checkUser($username,$password)){
-					$res = $testuser->getUserId($username);
-					$user_id = $res['user_id'];
-					if($user_id == $id){
-						$image_id = $this->model('Picture')->createPicture($username, $title, $image, $description);
-						echo json_encode($image_id, JSON_PRETTY_PRINT);
-					}
+				if ($testuser->checkUser($username,$password) && $testuser->getUserID($username) == $id){
+					$imageid = $this->model('Picture')->createPicture($username, $title, $image, $description);
+					echo '{"image_id": "'. $imageid .'"}';
 				}
 			}
 		}
