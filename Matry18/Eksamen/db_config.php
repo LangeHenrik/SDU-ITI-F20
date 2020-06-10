6a299822ae737cc872dc12146cf292d312bb9e1c
Mathias Tryggedsson <43138771+matry18@users.noreply.github.com>
2020-06-10 15:51:30 +0200

    pdo og prepared stmt

---
+++ Matry18/Eksamen/db_config.php
@@ -0,0 +1,6 @@
+<?php
+$servername = "localhost";
+$username = "Mathias";
+$password = "password";
+$dbname = "examDB";
+?>
---
+++ Matry18/Eksamen/index.php
@@ -0,0 +1,34 @@
+<?php
+require_once 'db_config.php';
+try {
+
+$conn = new PDO("mysql:host = $servername;dbname=$dbname",
+$username,
+$password,
+array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+
+$stmt = $conn->prepare("SHOW DATABASES;");
+
+$stmt->execute();
+$stmt->setFetchMode(PDO::FETCH_ASSOC);
+$result = $stmt->fetchAll();
+print_r($result);
+
+} catch (PDOException $e) {
+    echo "Error: ".$e->getMessage();
+}
+
+$conn = null;
+
+
+
+// prepare sql and bind parameters
+  $stmt = $conn->prepare("INSERT INTO Person (firstname, lastname, age)
+  VALUES (:firstname, :lastname, :age)");
+  $stmt->bindParam(':firstname', $firstName);
+  $stmt->bindParam(':lastname', $lastName);
+  $stmt->bindParam(':age', $age);
+
+  $stmt->execute();
+
+  ?>
